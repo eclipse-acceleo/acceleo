d86f97dab37445978dbaed9a46ec3f2ad9054a85
lgoubet <lgoubet>
2009-04-30 13:15:36 +0000

    Synching examples with the latest devs

--- examples/org.eclipse.acceleo.module.example.ecore2python.ui/src/org/eclipse/acceleo/module/example/ecore2python/ui/common/GenerateAll.java
+++ examples/org.eclipse.acceleo.module.example.ecore2python.ui/src/org/eclipse/acceleo/module/example/ecore2python/ui/common/GenerateAll.java
@@ -16,7 +16,9 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.List;
 
+import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
@@ -59,68 +61,63 @@ public class GenerateAll {
 	 * @generated
 	 */
 	public GenerateAll(URI modelURI, File targetFolder, List<? extends Object> arguments) {
-		this.modelURI = modelURI;
-		this.targetFolder = targetFolder;
-		this.arguments = arguments;
-	}
+    this.modelURI = modelURI;
+    this.targetFolder = targetFolder;
+    this.arguments = arguments;
+  }
 
 	/**
-	 * Launches the generation.
-	 * 
-	 * @throws IOException
-	 *             Thrown when the output cannot be saved.
-	 * @generated
-	 */
-	public void doGenerate() throws IOException {
-		if (!targetFolder.exists()) {
-			targetFolder.mkdirs();
-		}
+   * Launches the generation.
+   *
+   * @param monitor
+   *            This will be used to display progress information to the user.
+   * @throws IOException
+   *             Thrown when the output cannot be saved.
+   * @generated
+   */
+  public void doGenerate(IProgressMonitor monitor) throws IOException {
+    if (!targetFolder.exists()) {
+      targetFolder.mkdirs();
+    }
+    
+    final URI template0 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path("/org/eclipse/acceleo/module/ecore2python/commonClass.emtl"));
+    org.eclipse.acceleo.module.ecore2python.CommonClass gen0 = new org.eclipse.acceleo.module.ecore2python.CommonClass(modelURI, targetFolder, arguments) {
+      protected URI createTemplateURI(String entry) {
+        return template0;
+      }
+    };
+    gen0.doGenerate(BasicMonitor.toMonitor(monitor));
+      
+    EObject model = gen0.getModel();
+    if (model != null) {
+        
+      final URI template1 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path("/org/eclipse/acceleo/module/ecore2python/factory.emtl"));
+      org.eclipse.acceleo.module.ecore2python.Factory gen1 = new org.eclipse.acceleo.module.ecore2python.Factory(model, targetFolder, arguments) {
+        protected URI createTemplateURI(String entry) {
+          return template1;
+        }
+      };
+      gen1.doGenerate(BasicMonitor.toMonitor(monitor));
+      final URI template2 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path("/org/eclipse/acceleo/module/ecore2python/init.emtl"));
+      org.eclipse.acceleo.module.ecore2python.Init gen2 = new org.eclipse.acceleo.module.ecore2python.Init(model, targetFolder, arguments) {
+        protected URI createTemplateURI(String entry) {
+          return template2;
+        }
+      };
+      gen2.doGenerate(BasicMonitor.toMonitor(monitor));
+      final URI template3 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path("/org/eclipse/acceleo/module/ecore2python/parser.emtl"));
+      org.eclipse.acceleo.module.ecore2python.Parser gen3 = new org.eclipse.acceleo.module.ecore2python.Parser(model, targetFolder, arguments) {
+        protected URI createTemplateURI(String entry) {
+          return template3;
+        }
+      };
+      gen3.doGenerate(BasicMonitor.toMonitor(monitor));
+    }
+      
+    
+  }
 
-		final URI template0 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path(
-				"/org/eclipse/acceleo/module/ecore2python/commonClass.emtl"));
-		org.eclipse.acceleo.module.ecore2python.CommonClass gen0 = new org.eclipse.acceleo.module.ecore2python.CommonClass(
-				modelURI, targetFolder, arguments) {
-			protected URI createTemplateURI(String entry) {
-				return template0;
-			}
-		};
-		gen0.doGenerate();
-
-		EObject model = gen0.getModel();
-		if (model != null) {
-
-			final URI template1 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path(
-					"/org/eclipse/acceleo/module/ecore2python/factory.emtl"));
-			org.eclipse.acceleo.module.ecore2python.Factory gen1 = new org.eclipse.acceleo.module.ecore2python.Factory(
-					model, targetFolder, arguments) {
-				protected URI createTemplateURI(String entry) {
-					return template1;
-				}
-			};
-			gen1.doGenerate();
-			final URI template2 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path(
-					"/org/eclipse/acceleo/module/ecore2python/init.emtl"));
-			org.eclipse.acceleo.module.ecore2python.Init gen2 = new org.eclipse.acceleo.module.ecore2python.Init(
-					model, targetFolder, arguments) {
-				protected URI createTemplateURI(String entry) {
-					return template2;
-				}
-			};
-			gen2.doGenerate();
-			final URI template3 = getTemplateURI("org.eclipse.acceleo.module.example.ecore2python", new Path(
-					"/org/eclipse/acceleo/module/ecore2python/parser.emtl"));
-			org.eclipse.acceleo.module.ecore2python.Parser gen3 = new org.eclipse.acceleo.module.ecore2python.Parser(
-					model, targetFolder, arguments) {
-				protected URI createTemplateURI(String entry) {
-					return template3;
-				}
-			};
-			gen3.doGenerate();
-		}
-
-	}
-
-	/**
+  /**
 	 * Finds the template in the plug-in. Returns the template plug-in URI.
 	 * 
 	 * @param bundleID
@@ -133,41 +130,40 @@ public class GenerateAll {
 	 */
 	@SuppressWarnings("unchecked")
 	private URI getTemplateURI(String bundleID, IPath relativePath) throws IOException {
-		Bundle bundle = Platform.getBundle(bundleID);
-		if (bundle == null) {
-			// no need to go any further
-			return URI.createPlatformResourceURI(new Path(bundleID).append(relativePath).toString(), false);
-		}
-		URL url = bundle.getEntry(relativePath.toString());
-		if (url == null && relativePath.segmentCount() > 1) {
-			Enumeration<URL> entries = bundle.findEntries("/", "*.emtl", true);
-			if (entries != null) {
-				String[] segmentsRelativePath = relativePath.segments();
-				while (url == null && entries.hasMoreElements()) {
-					URL entry = entries.nextElement();
-					IPath path = new Path(entry.getPath());
-					if (path.segmentCount() > relativePath.segmentCount()) {
-						path = path.removeFirstSegments(path.segmentCount() - relativePath.segmentCount());
-					}
-					String[] segmentsPath = path.segments();
-					boolean equals = segmentsPath.length == segmentsRelativePath.length;
-					for (int i = 0; equals && i < segmentsPath.length; i++) {
-						equals = segmentsPath[i].equals(segmentsRelativePath[i]);
-					}
-					if (equals) {
-						url = bundle.getEntry(entry.getPath());
-					}
-				}
-			}
-		}
-		URI result;
-		if (url != null) {
-			result = URI.createPlatformPluginURI(new Path(bundleID).append(new Path(url.getPath()))
-					.toString(), false);
-		} else {
-			result = URI.createPlatformResourceURI(new Path(bundleID).append(relativePath).toString(), false);
-		}
-		return result;
-	}
+    Bundle bundle = Platform.getBundle(bundleID);
+    if (bundle == null) {
+      // no need to go any further
+      return URI.createPlatformResourceURI(new Path(bundleID).append(relativePath).toString(), false);
+    }
+    URL url = bundle.getEntry(relativePath.toString());
+    if (url == null && relativePath.segmentCount() > 1) {
+      Enumeration<URL> entries = bundle.findEntries("/", "*.emtl", true);
+      if (entries != null) {
+        String[] segmentsRelativePath = relativePath.segments();
+        while (url == null && entries.hasMoreElements()) {
+          URL entry = entries.nextElement();
+          IPath path = new Path(entry.getPath());
+          if (path.segmentCount() > relativePath.segmentCount()) {
+            path = path.removeFirstSegments(path.segmentCount() - relativePath.segmentCount());
+          }
+          String[] segmentsPath = path.segments();
+          boolean equals = segmentsPath.length == segmentsRelativePath.length;
+          for (int i = 0; equals && i < segmentsPath.length; i++) {
+            equals = segmentsPath[i].equals(segmentsRelativePath[i]);
+          }
+          if (equals) {
+            url = bundle.getEntry(entry.getPath());
+          }
+        }
+      }
+    }
+    URI result;
+    if (url != null) {
+      result = URI.createPlatformPluginURI(new Path(bundleID).append(new Path(url.getPath())).toString(), false);
+    } else {
+      result = URI.createPlatformResourceURI(new Path(bundleID).append(relativePath).toString(), false);
+    }
+    return result;
+  }
 
 }
