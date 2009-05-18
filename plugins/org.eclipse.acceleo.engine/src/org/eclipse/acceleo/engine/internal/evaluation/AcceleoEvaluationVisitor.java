a84d9da49605b55a1ec46c271654fcdbdb557e24
lgoubet <lgoubet>
2009-05-18 09:34:18 +0000

    [276667] NPE fixed from unit tests before the bug was raised

--- plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/internal/evaluation/AcceleoEvaluationVisitor.java
+++ plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/internal/evaluation/AcceleoEvaluationVisitor.java
@@ -870,6 +870,7 @@ public class AcceleoEvaluationVisitor<PK, C, O, P, EL, PM, S, COA, SSA, CT, CLS,
 	 *            Result of the evaluation from which we detected the operation cancelation.
 	 */
 	private void cancel(ASTFragment astFragment, EObject debugInput, Object result) {
+		// #276667 "debug" can be null
 		if (debug != null) {
 			debug.stepDebugOutput(astFragment, debugInput, result);
 			debug.endDebug(astFragment);
