d24a3204f4b55e4e8c475a1a40f8707b0501082d
jmusset <jmusset>
2009-05-19 15:26:25 +0000

    Options have been reverted in the Galileo Acceleo environment, FIXME LGO

--- tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/suite/AllTests.java
+++ tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/suite/AllTests.java
@@ -21,6 +21,7 @@ import org.eclipse.acceleo.engine.tests.unit.blocks.protectedareablock.Protected
 import org.eclipse.acceleo.engine.tests.unit.blocks.template.TemplateTest;
 import org.eclipse.acceleo.engine.tests.unit.blocks.templateinvocation.DualTemplateInvocationTest;
 import org.eclipse.acceleo.engine.tests.unit.blocks.templateinvocation.QueryPropertyInvocationTest;
+import org.eclipse.acceleo.engine.tests.unit.environment.AcceleoNonStandardLibraryParsedTest;
 import org.eclipse.acceleo.engine.tests.unit.environment.AcceleoNonStandardLibraryTest;
 import org.eclipse.acceleo.engine.tests.unit.environment.AcceleoStandardLibraryParsedTest;
 import org.eclipse.acceleo.engine.tests.unit.environment.AcceleoStandardLibraryTest;
@@ -105,13 +106,12 @@ public class AllTests extends TestCase {
 		suite.addTestSuite(AcceleoStandardLibraryTest.class);
 		suite.addTestSuite(AcceleoStandardLibraryParsedTest.class);
 		suite.addTestSuite(AcceleoNonStandardLibraryTest.class);
-		// FIXME LGO NPE
-		// try {
-		//			Class.forName("org.eclipse.ocl.TypeChecker"); //$NON-NLS-1$
-		// suite.addTestSuite(AcceleoNonStandardLibraryParsedTest.class);
-		// } catch (ClassNotFoundException e) {
-		// // OCL 1.3 isn't accessible in the classpath
-		// }
+		try {
+			Class.forName("org.eclipse.ocl.TypeChecker"); //$NON-NLS-1$
+			suite.addTestSuite(AcceleoNonStandardLibraryParsedTest.class);
+		} catch (ClassNotFoundException e) {
+			// OCL 1.3 isn't accessible in the classpath
+		}
 
 		return suite;
 	}
