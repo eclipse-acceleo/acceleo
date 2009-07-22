5d8270ff7e9f848d9b5a7c0dd3694d332084de29
jmusset <jmusset>
2009-07-22 10:48:36 +0000

    [282689] New: Compatibility plug-in to convert the templates from the old syntax to the new MTL syntax

---
+++ plugins/org.eclipse.acceleo.compatibility/src-gen/org/eclipse/acceleo/compatibility/model/mt/statements/Statement.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.compatibility.model.mt.statements;
+
+import org.eclipse.acceleo.compatibility.model.mt.core.ASTNode;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Statement</b></em>'. <!-- end-user-doc
+ * -->
+ * 
+ * @see org.eclipse.acceleo.compatibility.model.mt.statements.StatementsPackage#getStatement()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface Statement extends ASTNode {
+} // Statement
