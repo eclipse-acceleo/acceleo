f25995c6548978e52246c8ce61716cca9eb15d67
ylussaud <ylussaud>
2009-09-08 16:05:39 +0000

    Initial provisioning of project model.edit

---
+++ plugins/org.eclipse.acceleo.model.edit/src/org/eclipse/acceleo/model/mtl/provider/QueryInvocationItemProviderSpec.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.model.mtl.provider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+
+/**
+ * Specializes the QueryInvocationItemProvider implementation.
+ * 
+ * @author <a href="mailto:yvan.lussaud@obeo.fr">Yvan Lussaud</a>
+ */
+public class QueryInvocationItemProviderSpec extends QueryInvocationItemProvider {
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param adapterFactory
+	 *            the adapter factory
+	 */
+	public QueryInvocationItemProviderSpec(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.acceleo.model.mtl.provider.QueryInvocationItemProvider#getText(java.lang.Object)
+	 */
+	@Override
+	public String getText(Object object) {
+		return object.toString();
+	}
+}
---
+++ plugins/org.eclipse.acceleo.model.edit/src/org/eclipse/acceleo/model/mtl/provider/TemplateItemProviderSpec.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.model.mtl.provider;
+
+import java.util.Iterator;
+
+import org.eclipse.acceleo.model.mtl.Template;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.ocl.ecore.Variable;
+
+/**
+ * Specializes the TemplateInvocationItemProvider implementation.
+ * 
+ * @author <a href="mailto:yvan.lussaud@obeo.fr">Yvan Lussaud</a>
+ */
+public class TemplateItemProviderSpec extends TemplateItemProvider {
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param adapterFactory
+	 *            the adapter factory
+	 */
+	public TemplateItemProviderSpec(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.acceleo.model.mtl.provider.TemplateItemProvider#getText(java.lang.Object)
+	 */
+	@Override
+	public String getText(Object object) {
+		Template eTemplate = (Template)object;
+		StringBuffer text = new StringBuffer();
+		text.append("template "); //$NON-NLS-1$
+		text.append(eTemplate.getName());
+		text.append("("); //$NON-NLS-1$
+		for (Iterator<Variable> eParameters = eTemplate.getParameter().iterator(); eParameters.hasNext();) {
+			Variable eVariable = eParameters.next();
+			if (eVariable.getType() != null) {
+				text.append(eVariable.getType());
+			}
+			if (eParameters.hasNext()) {
+				text.append(", "); //$NON-NLS-1$
+			}
+		}
+		text.append(")"); //$NON-NLS-1$
+		return text.toString();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.acceleo.model.mtl.provider.TemplateInvocationItemProvider#getImage(java.lang.Object)
+	 */
+	@Override
+	public Object getImage(Object object) {
+		Object res = null;
+
+		if (((Template)object).isMain()) {
+			res = overlayImage(object, getResourceLocator().getImage("full/obj16/Template_main")); //$NON-NLS-1$
+		} else {
+			res = super.getImage(object);
+		}
+		return res;
+	}
+}
