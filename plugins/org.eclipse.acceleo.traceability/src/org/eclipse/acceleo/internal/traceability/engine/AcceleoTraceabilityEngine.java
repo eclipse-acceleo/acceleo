cbac56e0f31e0c015608f85437b0e784e7f5ae42
lgoubet <lgoubet>
2010-01-28 15:27:59 +0000

    *** empty log message ***

--- plugins/org.eclipse.acceleo.traceability/src/org/eclipse/acceleo/internal/traceability/engine/AcceleoTraceabilityEngine.java
+++ plugins/org.eclipse.acceleo.traceability/src/org/eclipse/acceleo/internal/traceability/engine/AcceleoTraceabilityEngine.java
@@ -11,13 +11,10 @@
 package org.eclipse.acceleo.internal.traceability.engine;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import org.eclipse.acceleo.common.utils.ModelUtils;
-import org.eclipse.acceleo.engine.AcceleoEnginePlugin;
 import org.eclipse.acceleo.engine.event.AcceleoTextGenerationEvent;
 import org.eclipse.acceleo.engine.event.IAcceleoTextGenerationListener;
 import org.eclipse.acceleo.engine.generation.AbstractAcceleoEnvironmentFactory;
@@ -36,15 +33,6 @@ import org.eclipse.emf.common.util.Monitor;
  * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
  */
 public class AcceleoTraceabilityEngine extends AcceleoEngine {
-	/**
-	 * Name that will be used for the generated traceability model. Note that this will be created within the
-	 * generation root directory.
-	 */
-	private static final String TRACEABILITY_MODEL_NAME = "generation";
-
-	/** Extension of the generated traceability model. */
-	private static final String TRACEABILITY_MODEL_EXTENSION = "trace";
-
 	/** All traceability information for this session will be saved in this instance. */
 	private TraceabilityModel evaluationTrace = TraceabilityFactory.eINSTANCE.createTraceabilityModel();
 
@@ -74,30 +62,6 @@ public class AcceleoTraceabilityEngine extends AcceleoEngine {
 	/**
 	 * {@inheritDoc}
 	 * 
-	 * @see org.eclipse.acceleo.engine.generation.AcceleoEngine#hookGenerationEnd(org.eclipse.acceleo.engine.generation.AbstractAcceleoEnvironmentFactory)
-	 */
-	@Override
-	protected void hookGenerationEnd(AbstractAcceleoEnvironmentFactory factory) {
-		super.hookGenerationEnd(factory);
-		if (generationRoot != null) {
-			int number = 0;
-			final String traceabilityModelPath = computeTraceabilityModelPath(generationRoot, number);
-			File traceabilityFile = new File(traceabilityModelPath);
-			while (traceabilityFile.exists()) {
-				traceabilityFile = new File(computeTraceabilityModelPath(generationRoot, ++number));
-			}
-			try {
-				ModelUtils.save(evaluationTrace, traceabilityModelPath);
-				evaluationTrace.eResource().unload();
-			} catch (IOException e) {
-				AcceleoEnginePlugin.log(e, false);
-			}
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * 
 	 * @see org.eclipse.acceleo.engine.generation.AcceleoEngine#fireGenerationEnd()
 	 */
 	@Override
@@ -122,29 +86,5 @@ public class AcceleoTraceabilityEngine extends AcceleoEngine {
 		super.reset();
 		generationRoot = null;
 		evaluationTrace = null;
-	}
-
-	/**
-	 * Returns an absolute path name according to the given parameters. The generated path will look like
-	 * 
-	 * <em>&lt;root path&gt;&lt;file separator char&gt;&lt;TRACEABILITY_MODEL_NAME&gt;&lt;number&gt;.&lt;TRACEABILITY_MODEL_EXTENSION&gt;</em>
-	 * 
-	 * @param root
-	 *            Current generation root.
-	 * @param number
-	 *            Number that is to be appended at the end of the model name, in case a traceability model
-	 *            already exists.
-	 * @return The computed path.
-	 */
-	private static String computeTraceabilityModelPath(File root, int number) {
-		final StringBuilder buffer = new StringBuilder(root.getAbsolutePath());
-		buffer.append(File.separatorChar);
-		buffer.append(TRACEABILITY_MODEL_NAME);
-		if (number > 0) {
-			buffer.append(number);
-		}
-		buffer.append('.');
-		buffer.append(TRACEABILITY_MODEL_EXTENSION);
-		return buffer.toString();
 	}
 }
