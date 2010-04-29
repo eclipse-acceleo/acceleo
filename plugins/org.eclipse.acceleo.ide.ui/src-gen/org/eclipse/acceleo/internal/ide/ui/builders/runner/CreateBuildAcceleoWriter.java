cf811147d20d555031edd8c4c1199aef518b5890
jmusset <jmusset>
2010-04-29 09:37:19 +0000

    Acceleo build file dependencies support

--- plugins/org.eclipse.acceleo.ide.ui/src-gen/org/eclipse/acceleo/internal/ide/ui/builders/runner/CreateBuildAcceleoWriter.java
+++ plugins/org.eclipse.acceleo.ide.ui/src-gen/org/eclipse/acceleo/internal/ide/ui/builders/runner/CreateBuildAcceleoWriter.java
@@ -12,18 +12,26 @@ public class CreateBuildAcceleoWriter
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "<!-- ===================================================================== -->" + NL + "<!-- Custom targets.                                                       -->" + NL + "<!-- Set customBuildCallbacks = build.acceleo in your build.properties.    -->" + NL + "<!-- ===================================================================== -->" + NL + "<project name=\"Build Acceleo Module\" default=\"run\">" + NL + "" + NL + "\t<!-- ================================================================= -->" + NL + "\t<!-- Steps to do after the target gather.bin.parts                     -->" + NL + "\t<!-- Available parameters :                                            -->" + NL + "\t<!--   build.result.folder - folder containing the build results       -->" + NL + "\t<!--   target.folder - destination folder                              -->" + NL + "\t<!-- ================================================================= -->" + NL + "" + NL + "\t<target name=\"post.gather.bin.parts\">" + NL + "\t\t<acceleoCompiler sourceFolders=\"${target.folder}\"/>" + NL + "\t</target>" + NL + "" + NL + "</project>";
-  protected final String TEXT_2 = NL;
+  protected final String TEXT_1 = "<!-- ===================================================================== -->" + NL + "<!-- Custom targets.                                                       -->" + NL + "<!-- Set customBuildCallbacks = build.acceleo in your build.properties.    -->" + NL + "<!-- ===================================================================== -->" + NL + "<project name=\"Build Acceleo Module\" default=\"noDefault\">" + NL + "" + NL + "\t<!-- ================================================================= -->" + NL + "\t<!-- Default target                                                    -->" + NL + "\t<!-- ================================================================= -->" + NL + "\t<target name=\"noDefault\">" + NL + "\t\t<echo message=\"This file must be called with explicit targets\" />" + NL + "\t</target>" + NL + "" + NL + "\t<!-- ================================================================= -->" + NL + "\t<!-- Steps to do after the target gather.bin.parts                     -->" + NL + "\t<!-- Available parameters :                                            -->" + NL + "\t<!--   build.result.folder - folder containing the build results       -->" + NL + "\t<!--   target.folder - destination folder                              -->" + NL + "\t<!-- ================================================================= -->" + NL + "" + NL + "\t<target name=\"post.gather.bin.parts\">" + NL + "\t\t<acceleoCompiler " + NL + "\t\t\tsourceFolders=\"${target.folder}\" " + NL + "\t\t\tdependencies=\"";
+  protected final String TEXT_2 = "${target.folder}/../";
+  protected final String TEXT_3 = ";";
+  protected final String TEXT_4 = "\">" + NL + "\t\t</acceleoCompiler>" + NL + "\t</target>\t" + NL + "" + NL + "</project>";
+  protected final String TEXT_5 = NL;
 
   public String generate(Object argument)
   {
     final StringBuffer stringBuffer = new StringBuffer();
     
-// String projectName = (String) ((Object[])argument)[0];
-// String outputFolder = (String) ((Object[])argument)[1];
+java.util.List<org.eclipse.core.resources.IProject> dependencies = (java.util.List<org.eclipse.core.resources.IProject>) ((Object[])argument)[0];
 
     stringBuffer.append(TEXT_1);
+    for (org.eclipse.core.resources.IProject project : dependencies) {
     stringBuffer.append(TEXT_2);
+    stringBuffer.append(project.getName());
+    stringBuffer.append(TEXT_3);
+    }
+    stringBuffer.append(TEXT_4);
+    stringBuffer.append(TEXT_5);
     return stringBuffer.toString();
   }
 }
