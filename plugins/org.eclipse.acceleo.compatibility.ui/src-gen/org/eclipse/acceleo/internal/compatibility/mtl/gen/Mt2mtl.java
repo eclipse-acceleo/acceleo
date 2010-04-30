b8b272323a012f7f2601be35e10705383b4b2fa6
lgoubet <lgoubet>
2010-04-30 10:01:00 +0000

    Organizing imports

--- plugins/org.eclipse.acceleo.compatibility.ui/src-gen/org/eclipse/acceleo/internal/compatibility/mtl/gen/Mt2mtl.java
+++ plugins/org.eclipse.acceleo.compatibility.ui/src-gen/org/eclipse/acceleo/internal/compatibility/mtl/gen/Mt2mtl.java
@@ -12,35 +12,17 @@ package org.eclipse.acceleo.internal.compatibility.mtl.gen;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.acceleo.engine.event.IAcceleoTextGenerationListener;
-import org.eclipse.acceleo.engine.generation.strategy.DefaultStrategy;
 import org.eclipse.acceleo.engine.generation.strategy.IAcceleoGenerationStrategy;
 import org.eclipse.acceleo.engine.service.AbstractAcceleoGenerator;
-import org.eclipse.acceleo.engine.service.AcceleoService;
-import org.eclipse.acceleo.model.mtl.Module;
-import org.eclipse.acceleo.model.mtl.MtlPackage;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.Monitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.ocl.ecore.EcoreEnvironment;
-import org.eclipse.ocl.ecore.EcoreEnvironmentFactory;
 
 /**
  * Entry point of the 'Mt2mtl' generation module.
@@ -62,27 +44,25 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * 
 	 * @generated
 	 */
-	public static final String[] TEMPLATE_NAMES = { "convertToModule", };
+	public static final String[] TEMPLATE_NAMES = {"convertToModule", };
 
 	/**
-   * Allows the public constructor to be used. Note that a generator created
-   * this way cannot be used to launch generations before one of
-   * {@link #initialize(EObject, File, List)} or
-   * {@link #initialize(URI, File, List)} is called.
-   * <p>
-   * The main reason for this constructor is to allow clients of this
-   * generation to call it from another Java file, as it allows for the
-   * retrieval of {@link #getProperties()} and
-   * {@link #getGenerationListeners()}.
-   * </p>
-   *
-   * @generated
-   */
-  public Mt2mtl() {
-    // Empty implementation
-  }
+	 * Allows the public constructor to be used. Note that a generator created this way cannot be used to
+	 * launch generations before one of {@link #initialize(EObject, File, List)} or
+	 * {@link #initialize(URI, File, List)} is called.
+	 * <p>
+	 * The main reason for this constructor is to allow clients of this generation to call it from another
+	 * Java file, as it allows for the retrieval of {@link #getProperties()} and
+	 * {@link #getGenerationListeners()}.
+	 * </p>
+	 * 
+	 * @generated
+	 */
+	public Mt2mtl() {
+		// Empty implementation
+	}
 
-  /**
+	/**
 	 * Constructor.
 	 * 
 	 * @param modelURI
@@ -96,8 +76,8 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * @generated
 	 */
 	public Mt2mtl(URI modelURI, File targetFolder, List<? extends Object> arguments) throws IOException {
-    initialize(modelURI, targetFolder, arguments);
-  }
+		initialize(modelURI, targetFolder, arguments);
+	}
 
 	/**
 	 * Constructor.
@@ -113,8 +93,8 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * @generated
 	 */
 	public Mt2mtl(EObject model, File targetFolder, List<? extends Object> arguments) throws IOException {
-    initialize(model, targetFolder, arguments);
-  }
+		initialize(model, targetFolder, arguments);
+	}
 
 	/**
 	 * Updates the registry used for looking up a package based namespace, in the resource set.
@@ -124,15 +104,24 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * @generated
 	 */
 	@Override
-  public void registerPackages(ResourceSet resourceSet) {
-    super.registerPackages(resourceSet);
-    resourceSet.getPackageRegistry().put(org.eclipse.acceleo.compatibility.model.mt.MtPackage.eINSTANCE.getNsURI(), org.eclipse.acceleo.compatibility.model.mt.MtPackage.eINSTANCE);
-    resourceSet.getPackageRegistry().put(org.eclipse.acceleo.compatibility.model.mt.core.CorePackage.eINSTANCE.getNsURI(), org.eclipse.acceleo.compatibility.model.mt.core.CorePackage.eINSTANCE);
-    resourceSet.getPackageRegistry().put(org.eclipse.acceleo.compatibility.model.mt.expressions.ExpressionsPackage.eINSTANCE.getNsURI(), org.eclipse.acceleo.compatibility.model.mt.expressions.ExpressionsPackage.eINSTANCE);
-    resourceSet.getPackageRegistry().put(org.eclipse.acceleo.compatibility.model.mt.statements.StatementsPackage.eINSTANCE.getNsURI(), org.eclipse.acceleo.compatibility.model.mt.statements.StatementsPackage.eINSTANCE);
-    // TODO Uncomment the following line if generating from UML models
-    // resourceSet.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
-  }
+	public void registerPackages(ResourceSet resourceSet) {
+		super.registerPackages(resourceSet);
+		resourceSet.getPackageRegistry().put(
+				org.eclipse.acceleo.compatibility.model.mt.MtPackage.eINSTANCE.getNsURI(),
+				org.eclipse.acceleo.compatibility.model.mt.MtPackage.eINSTANCE);
+		resourceSet.getPackageRegistry().put(
+				org.eclipse.acceleo.compatibility.model.mt.core.CorePackage.eINSTANCE.getNsURI(),
+				org.eclipse.acceleo.compatibility.model.mt.core.CorePackage.eINSTANCE);
+		resourceSet.getPackageRegistry().put(
+				org.eclipse.acceleo.compatibility.model.mt.expressions.ExpressionsPackage.eINSTANCE
+						.getNsURI(),
+				org.eclipse.acceleo.compatibility.model.mt.expressions.ExpressionsPackage.eINSTANCE);
+		resourceSet.getPackageRegistry().put(
+				org.eclipse.acceleo.compatibility.model.mt.statements.StatementsPackage.eINSTANCE.getNsURI(),
+				org.eclipse.acceleo.compatibility.model.mt.statements.StatementsPackage.eINSTANCE);
+		// TODO Uncomment the following line if generating from UML models
+		// resourceSet.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+	}
 
 	/**
 	 * Updates the registry used for looking up resources factory in the given resource set.
@@ -142,11 +131,12 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * @generated
 	 */
 	@Override
-  public void registerResourceFactories(ResourceSet resourceSet) {
-    super.registerResourceFactories(resourceSet);
-    // TODO Uncomment the following line if generating from UML models
-    // resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(UMLResource.FILE_EXTENSION, UMLResource.Factory.INSTANCE);
-  }
+	public void registerResourceFactories(ResourceSet resourceSet) {
+		super.registerResourceFactories(resourceSet);
+		// TODO Uncomment the following line if generating from UML models
+		// resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(UMLResource.FILE_EXTENSION,
+		// UMLResource.Factory.INSTANCE);
+	}
 
 	/**
 	 * The main method.
@@ -156,24 +146,24 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * @generated
 	 */
 	public static void main(String[] args) {
-    try {
-      if (args.length < 2) {
-        System.out.println("Arguments not valid : {model, folder}.");
-      } else {
-        URI modelURI = URI.createFileURI(args[0]);
-        File folder = new File(args[1]);
-        List<String> arguments = new ArrayList<String>();
-        for (int i = 2; i < args.length; i++) {
-          arguments.add(args[i]);
-        }
-        Mt2mtl generator = new Mt2mtl();
-        generator.initialize(modelURI, folder, arguments);
-        generator.doGenerate(new BasicMonitor());
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+		try {
+			if (args.length < 2) {
+				System.out.println("Arguments not valid : {model, folder}.");
+			} else {
+				URI modelURI = URI.createFileURI(args[0]);
+				File folder = new File(args[1]);
+				List<String> arguments = new ArrayList<String>();
+				for (int i = 2; i < args.length; i++) {
+					arguments.add(args[i]);
+				}
+				Mt2mtl generator = new Mt2mtl();
+				generator.initialize(modelURI, folder, arguments);
+				generator.doGenerate(new BasicMonitor());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Launches the generation.
@@ -185,28 +175,28 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * @generated
 	 */
 	@Override
-  public void doGenerate(Monitor monitor) throws IOException {
-    /*
-     * TODO if you wish to change the generation as a whole, override this.
-     * The default behavior should be sufficient in most cases.
-     */
-    super.doGenerate(monitor);
-  }
+	public void doGenerate(Monitor monitor) throws IOException {
+		/*
+		 * TODO if you wish to change the generation as a whole, override this. The default behavior should be
+		 * sufficient in most cases.
+		 */
+		super.doGenerate(monitor);
+	}
 
 	/**
-   * If this generator needs to listen to text generation events, listeners can be returned from here.
-   * 
-   * @return List of listeners that are to be notified when text is generated through this launch.
-   * @generated
-   */
-  @Override
-  public List<IAcceleoTextGenerationListener> getGenerationListeners() {
-    List<IAcceleoTextGenerationListener> listeners = super.getGenerationListeners();
-    // TODO if you need to listen to generation event, add listeners to the list here
-    return listeners;
-  }
+	 * If this generator needs to listen to text generation events, listeners can be returned from here.
+	 * 
+	 * @return List of listeners that are to be notified when text is generated through this launch.
+	 * @generated
+	 */
+	@Override
+	public List<IAcceleoTextGenerationListener> getGenerationListeners() {
+		List<IAcceleoTextGenerationListener> listeners = super.getGenerationListeners();
+		// TODO if you need to listen to generation event, add listeners to the list here
+		return listeners;
+	}
 
-  /**
+	/**
 	 * If you need to change the way files are generated, this is your entry point.
 	 * <p>
 	 * The default is {@link org.eclipse.acceleo.engine.generation.strategy.DefaultStrategy}; it generates
@@ -226,61 +216,60 @@ public class Mt2mtl extends AbstractAcceleoGenerator {
 	 * 
 	 * @generated
 	 */
+	@Override
 	public IAcceleoGenerationStrategy getGenerationStrategy() {
-    return super.getGenerationStrategy();
-  }
+		return super.getGenerationStrategy();
+	}
 
 	/**
-   * This will be called in order to find and load the module that will be launched through this launcher.
-   * We expect this name not to contain file extension, and the module to be located beside the launcher.
-   * 
-   * @return The name of the module that is to be launched.
-   * @generated
-   */
-  @Override
-  public String getModuleName() {
-    return MODULE_FILE_NAME;
-  }
+	 * This will be called in order to find and load the module that will be launched through this launcher.
+	 * We expect this name not to contain file extension, and the module to be located beside the launcher.
+	 * 
+	 * @return The name of the module that is to be launched.
+	 * @generated
+	 */
+	@Override
+	public String getModuleName() {
+		return MODULE_FILE_NAME;
+	}
 
-  /**
-   * If the module(s) called by this launcher require properties files, return their qualified path from
-   * here.Take note that the first added properties files will take precedence over subsequent ones if they
-   * contain conflicting keys.
-   * <p>
-   * Properties need to be in source folders, the path that we expect to get as a result of this call are of
-   * the form &lt;package>.&lt;properties file name without extension>. For example, if you have a file
-   * named "messages.properties" in package "org.eclipse.acceleo.sample", the path that needs be returned by
-   * a call to {@link #getProperties()} is "org.eclipse.acceleo.sample.messages".
-   * </p>
-   * 
-   * @return The list of properties file we need to add to the generation context.
-   * @see java.util.ResourceBundle#getBundle(String)
-   * @generated
-   */
-  @Override
-  public List<String> getProperties() {
-    List<String> propertiesFiles = super.getProperties();
-    /*
-     * TODO if your generation module requires access to properties files,
-     * add their qualified path to the list here. Properties files are
-     * expected to be in source folders, and the path here to be the
-     * qualified path as if referring to a Java class. For example, if you
-     * have a file named "messages.properties" in package
-     * "org.eclipse.acceleo.sample", the path that needs be added to this
-     * list is "org.eclipse.acceleo.sample.messages".
-     */
-    return propertiesFiles;
-  }
+	/**
+	 * If the module(s) called by this launcher require properties files, return their qualified path from
+	 * here.Take note that the first added properties files will take precedence over subsequent ones if they
+	 * contain conflicting keys.
+	 * <p>
+	 * Properties need to be in source folders, the path that we expect to get as a result of this call are of
+	 * the form &lt;package>.&lt;properties file name without extension>. For example, if you have a file
+	 * named "messages.properties" in package "org.eclipse.acceleo.sample", the path that needs be returned by
+	 * a call to {@link #getProperties()} is "org.eclipse.acceleo.sample.messages".
+	 * </p>
+	 * 
+	 * @return The list of properties file we need to add to the generation context.
+	 * @see java.util.ResourceBundle#getBundle(String)
+	 * @generated
+	 */
+	@Override
+	public List<String> getProperties() {
+		List<String> propertiesFiles = super.getProperties();
+		/*
+		 * TODO if your generation module requires access to properties files, add their qualified path to the
+		 * list here. Properties files are expected to be in source folders, and the path here to be the
+		 * qualified path as if referring to a Java class. For example, if you have a file named
+		 * "messages.properties" in package "org.eclipse.acceleo.sample", the path that needs be added to this
+		 * list is "org.eclipse.acceleo.sample.messages".
+		 */
+		return propertiesFiles;
+	}
 
-  /**
-   * This will be used to get the list of templates that are to be launched by this launcher.
-   * 
-   * @return The list of templates to call on the module {@link #getModuleName()}.
-   * @generated
-   */
-  @Override
-  public String[] getTemplateNames() {
-    return TEMPLATE_NAMES;
-  }
+	/**
+	 * This will be used to get the list of templates that are to be launched by this launcher.
+	 * 
+	 * @return The list of templates to call on the module {@link #getModuleName()}.
+	 * @generated
+	 */
+	@Override
+	public String[] getTemplateNames() {
+		return TEMPLATE_NAMES;
+	}
 
 }
