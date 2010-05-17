a3c8a1fcb8e12e697661bf06bf0648728e3bb9b5
lgoubet <lgoubet>
2010-05-17 15:41:53 +0000

    We'll use a second example project for UML 3.0 compatibility as the URI changed and we d'ont want compilation errors in the user's workspace

---
+++ examples/org.eclipse.acceleo.module.example.uml2java.helios/src/org/eclipse/acceleo/module/example/uml2java/helios/Uml2JavaPlugin.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.module.example.uml2java.helios;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author <a href="mailto:stephane.bouchet@obeo.fr">Stephane Bouchet</a>
+ */
+public class Uml2JavaPlugin extends Plugin {
+
+	/**
+	 * The plug-in ID.
+	 */
+	public static final String PLUGIN_ID = "org.eclipse.acceleo.module.example.uml2java";
+
+	/**
+	 * The shared instance.
+	 */
+	private static Uml2JavaPlugin plugin;
+
+	/**
+	 * The constructor.
+	 */
+	public Uml2JavaPlugin() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 * 
+	 * @return the shared instance
+	 */
+	public static Uml2JavaPlugin getDefault() {
+		return plugin;
+	}
+
+}
