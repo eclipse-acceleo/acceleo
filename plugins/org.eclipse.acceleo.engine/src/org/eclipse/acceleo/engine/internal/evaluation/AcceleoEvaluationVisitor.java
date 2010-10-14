c3e9b0d7e99eb5cf6ef398545714feace48a789b
lgoubet <lgoubet>
2010-10-14 08:51:31 +0000

    Reverting, Collections can contain "invalid"

--- plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/internal/evaluation/AcceleoEvaluationVisitor.java
+++ plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/internal/evaluation/AcceleoEvaluationVisitor.java
@@ -1241,17 +1241,7 @@ public class AcceleoEvaluationVisitor<PK, C, O, P, EL, PM, S, COA, SSA, CT, CLS,
 	 *         <code>false</code> otherwise.
 	 */
 	private boolean isInvalid(Object value) {
-		boolean result = true;
-		if (value instanceof ArrayList<?>) {
-			@SuppressWarnings("unchecked")
-			ArrayList<Object> list = (ArrayList<Object>)value;
-			for (Object object : list) {
-				result = result && object == invalid;
-			}
-		} else {
-			result = value == invalid;
-		}
-		return result;
+		return value == invalid;
 	}
 
 	/**
