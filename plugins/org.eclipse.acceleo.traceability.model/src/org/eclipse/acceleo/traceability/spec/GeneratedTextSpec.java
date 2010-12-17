975f96198aa371b23fc66b22890ae76b1f502742
lgoubet <lgoubet>
2010-12-17 09:52:01 +0000

    Synching major bug fixes with HEAD

--- plugins/org.eclipse.acceleo.traceability.model/src/org/eclipse/acceleo/traceability/spec/GeneratedTextSpec.java
+++ plugins/org.eclipse.acceleo.traceability.model/src/org/eclipse/acceleo/traceability/spec/GeneratedTextSpec.java
@@ -10,7 +10,10 @@
  *******************************************************************************/
 package org.eclipse.acceleo.traceability.spec;
 
+import org.eclipse.acceleo.traceability.GeneratedFile;
 import org.eclipse.acceleo.traceability.GeneratedText;
+import org.eclipse.acceleo.traceability.InputElement;
+import org.eclipse.acceleo.traceability.ModuleElement;
 import org.eclipse.acceleo.traceability.impl.GeneratedTextImpl;
 
 /**
@@ -20,6 +23,64 @@ import org.eclipse.acceleo.traceability.impl.GeneratedTextImpl;
  * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
  */
 public class GeneratedTextSpec extends GeneratedTextImpl implements Comparable<GeneratedText> {
+	/** Cache the hash code, we'll reinitialize this cache whenever one of the fields change. */
+	private transient int hashCode;
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see GeneratedTextImpl#setEndOffset(int)
+	 */
+	@Override
+	public void setEndOffset(int newEndOffset) {
+		hashCode = 0;
+		super.setEndOffset(newEndOffset);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see GeneratedTextImpl#setModuleElement(ModuleElement)
+	 */
+	@Override
+	public void setModuleElement(ModuleElement newModuleElement) {
+		hashCode = 0;
+		super.setModuleElement(newModuleElement);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see GeneratedTextImpl#setOutputFile(GeneratedFile)
+	 */
+	@Override
+	public void setOutputFile(GeneratedFile newOutputFile) {
+		hashCode = 0;
+		super.setOutputFile(newOutputFile);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see GeneratedTextImpl#setSourceElement(InputElement)
+	 */
+	@Override
+	public void setSourceElement(InputElement newSourceElement) {
+		hashCode = 0;
+		super.setSourceElement(newSourceElement);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see GeneratedTextImpl#setStartOffset(int)
+	 */
+	@Override
+	public void setStartOffset(int newStartOffset) {
+		hashCode = 0;
+		super.setStartOffset(newStartOffset);
+	}
+
 	/**
 	 * {@inheritDoc}
 	 * 
@@ -44,5 +105,77 @@ public class GeneratedTextSpec extends GeneratedTextImpl implements Comparable<G
 	@Override
 	public int compareTo(GeneratedText o) {
 		return getStartOffset() - o.getStartOffset();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		if (hashCode != 0) {
+			return hashCode;
+		}
+
+		final int prime = 31;
+		hashCode = prime;
+
+		int tmp = startOffset << prime;
+		hashCode = prime * hashCode + (startOffset * tmp);
+		if (moduleElement == null) {
+			hashCode = prime * hashCode;
+		} else {
+			hashCode = prime * hashCode + moduleElement.hashCode();
+		}
+		if (getOutputFile() == null) {
+			hashCode = prime * hashCode;
+		} else {
+			hashCode = prime * hashCode + getOutputFile().hashCode();
+		}
+		if (sourceElement == null) {
+			hashCode = prime * hashCode;
+		} else {
+			hashCode = prime * hashCode + sourceElement.hashCode();
+		}
+		tmp = endOffset << prime;
+		hashCode = prime * hashCode + (endOffset * tmp);
+
+		return hashCode;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see java.lang.Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(Object other) {
+		boolean result = this == other;
+		if (!result && other instanceof GeneratedText) {
+			if (((GeneratedText)other).getStartOffset() == startOffset
+					&& ((GeneratedText)other).getEndOffset() == endOffset) {
+				result = true;
+				if (getOutputFile() == null) {
+					result = ((GeneratedText)other).getOutputFile() == null;
+				} else {
+					GeneratedFile otherOutputFile = ((GeneratedText)other).getOutputFile();
+					result = otherOutputFile != null && otherOutputFile.equals(getOutputFile());
+				}
+				if (result && moduleElement == null) {
+					result = ((GeneratedText)other).getModuleElement() == null;
+				} else if (result) {
+					ModuleElement otherModuleElement = ((GeneratedText)other).getModuleElement();
+					result = otherModuleElement != null && otherModuleElement.equals(moduleElement);
+				}
+				if (result && sourceElement == null) {
+					result = ((GeneratedText)other).getSourceElement() == null;
+				} else if (result) {
+					InputElement otherSourceElement = ((GeneratedText)other).getSourceElement();
+					result = otherSourceElement != null && otherSourceElement.equals(sourceElement);
+				}
+			}
+		}
+		return result;
 	}
 }
