a7cdf1e6a2a54e701180a2fc51ff9cf82f4dcf88
lgoubet <lgoubet>
2011-01-03 15:44:27 +0000

    Don't instantiate a service class when not needed

--- plugins/org.eclipse.acceleo.common/src/org/eclipse/acceleo/common/AcceleoServicesRegistry.java
+++ plugins/org.eclipse.acceleo.common/src/org/eclipse/acceleo/common/AcceleoServicesRegistry.java
@@ -130,7 +130,8 @@ public final class AcceleoServicesRegistry {
 		Class<?> clazz = null;
 		if (EMFPlugin.IS_ECLIPSE_RUNNING) {
 			clazz = AcceleoServicesEclipseUtil.registerService(bundleName, qualifiedName);
-		} else {
+		}
+		if (clazz == null) {
 			try {
 				clazz = Class.forName(qualifiedName);
 				if (clazz != null) {
@@ -196,15 +197,18 @@ public final class AcceleoServicesRegistry {
 		Class<?> clazz = null;
 		if (EMFPlugin.IS_ECLIPSE_RUNNING) {
 			clazz = AcceleoServicesEclipseUtil.registerService(uri, qualifiedName);
-		} else {
+		}
+		if (clazz == null) {
 			try {
 				clazz = Class.forName(qualifiedName);
 				if (clazz != null) {
 					registeredServices.add(clazz);
 				}
 			} catch (ClassNotFoundException e) {
-				AcceleoCommonPlugin.log(AcceleoCommonMessages.getString(
-						"AcceleoServicesRegistry.ClassLookupFailure", qualifiedName), e, true); //$NON-NLS-1$
+				if (!EMFPlugin.IS_ECLIPSE_RUNNING) {
+					AcceleoCommonPlugin.log(AcceleoCommonMessages.getString(
+							"AcceleoServicesRegistry.ClassLookupFailure", qualifiedName), e, true); //$NON-NLS-1$
+				}
 			}
 		}
 		return clazz;
