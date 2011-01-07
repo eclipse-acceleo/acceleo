26e707b93fcdc3fbda182bc4cb835b2035035228
lgoubet <lgoubet>
2011-01-07 08:28:41 +0000

    Copyrighting the generated sources

--- plugins/org.eclipse.acceleo.parser/src-gen/org/eclipse/acceleo/parser/cst/Documentation.java
+++ plugins/org.eclipse.acceleo.parser/src-gen/org/eclipse/acceleo/parser/cst/Documentation.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id: Documentation.java,v 1.2 2010/08/13 13:54:01 lgoubet Exp $
- */
+/*******************************************************************************
+ * Copyright (c) 2008, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.acceleo.parser.cst;
 
 /**
