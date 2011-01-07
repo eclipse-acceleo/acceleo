35d2dea3d380fcd8c113ad97da8eabe9bc22ff5e
lgoubet <lgoubet>
2011-01-07 08:28:40 +0000

    Copyrighting the generated sources

--- plugins/org.eclipse.acceleo.profiler.edit/src-gen/org/eclipse/acceleo/profiler/provider/LoopProfileEntryItemProvider.java
+++ plugins/org.eclipse.acceleo.profiler.edit/src-gen/org/eclipse/acceleo/profiler/provider/LoopProfileEntryItemProvider.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id: LoopProfileEntryItemProvider.java,v 1.2 2010/04/12 09:35:59 lgoubet Exp $
- */
+/*******************************************************************************
+ * Copyright (c) 2008, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.acceleo.profiler.provider;
 
 import java.text.DecimalFormat;
