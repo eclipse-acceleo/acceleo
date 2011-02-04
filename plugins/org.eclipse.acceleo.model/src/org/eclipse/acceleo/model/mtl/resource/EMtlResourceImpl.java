587d70177c0b1b157383adb006b95aecb50e9284
lgoubet <lgoubet>
2011-02-04 16:24:38 +0000

    Force option "record unknown features" so that we can load emtl files compiled with Acceleo 3.1 in Acceleo 3.0

--- plugins/org.eclipse.acceleo.model/src/org/eclipse/acceleo/model/mtl/resource/EMtlResourceImpl.java
+++ plugins/org.eclipse.acceleo.model/src/org/eclipse/acceleo/model/mtl/resource/EMtlResourceImpl.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -72,7 +72,7 @@ public class EMtlResourceImpl extends XMIResourceImpl {
 	public void doLoad(InputStream inputStream, Map<?, ?> options) throws IOException {
 		Map<Object, Object> actualOptions = (Map<Object, Object>)options;
 		if (actualOptions == null) {
-			actualOptions = Collections.EMPTY_MAP;
+			actualOptions = new HashMap<Object, Object>();
 		}
 		// Record unknown features so that we can load emtl files compiled under 3.1 with 3.0
 		actualOptions.put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
