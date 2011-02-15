6df787703425d1a0de40f69f721b26348d433e25
lgoubet <lgoubet>
2011-02-15 15:51:12 +0000

    [336597] We now handle proxified objets in the result view. Trying to merge erroneous Java files containing "@generated" annotations through JMerge no longer brutally fails, lost files are created in such cases.

--- plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/internal/evaluation/JMergeUtil.java
+++ plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/internal/evaluation/JMergeUtil.java
@@ -12,8 +12,13 @@ package org.eclipse.acceleo.engine.internal.evaluation;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Calendar;
 
+import org.eclipse.acceleo.common.IAcceleoConstants;
 import org.eclipse.acceleo.engine.AcceleoEngineMessages;
 import org.eclipse.acceleo.engine.AcceleoEnginePlugin;
 import org.eclipse.emf.codegen.merge.java.JControlModel;
@@ -30,6 +35,9 @@ import org.eclipse.emf.common.util.WrappedException;
  * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
  */
 public final class JMergeUtil {
+	/** This will hold the system specific line separator ("\n" for unix, "\r\n" for dos, "\r" for mac, ...). */
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator"); //$NON-NLS-1$
+
 	/**
 	 * Utility classes don't need to be instantiated.
 	 */
@@ -76,6 +84,12 @@ public final class JMergeUtil {
 					jMerger.merge();
 					newContent = jMerger.getTargetCompilationUnit().getContents();
 				} catch (WrappedException e) {
+					// The Java file contains errors. We'll copy the old file as a ".lost"
+					try {
+						createLostFile(target);
+					} catch (IOException ee) {
+						// FIXME log Couldn't create the lost file.
+					}
 					AcceleoEnginePlugin.log(AcceleoEngineMessages.getString(
 							"JMergeUtilError", target.getName()), false); //$NON-NLS-1$
 				}
@@ -84,6 +98,43 @@ public final class JMergeUtil {
 			}
 		}
 		return newContent;
+	}
+
+	/**
+	 * This will copy the given file as a ".lost" file in the same folder with the same name.
+	 * 
+	 * @param file
+	 *            The file we are to copy as a lost file.
+	 * @throws IOException
+	 *             Thrown if we couldn't read the source file or create its ".lost" sibling.
+	 */
+	private static void createLostFile(File file) throws IOException {
+		FileChannel source = null;
+		FileChannel destination = null;
+
+		try {
+			source = new FileInputStream(file).getChannel();
+			destination = new FileOutputStream(file.toString().concat(
+					IAcceleoConstants.ACCELEO_LOST_FILE_EXTENSION), true).getChannel();
+
+			// Print a time stamp of the current copy
+			StringBuilder timestamp = new StringBuilder();
+			timestamp.append(LINE_SEPARATOR).append(Calendar.getInstance().getTime().toString()).append(
+					LINE_SEPARATOR);
+			timestamp
+					.append("================================================================================"); //$NON-NLS-1$
+			timestamp.append(LINE_SEPARATOR);
+
+			destination.write(ByteBuffer.wrap(timestamp.toString().getBytes()));
+			source.transferTo(0, source.size(), destination);
+		} finally {
+			if (destination != null) {
+				destination.close();
+			}
+			if (source != null) {
+				source.close();
+			}
+		}
 	}
 
 	/**
