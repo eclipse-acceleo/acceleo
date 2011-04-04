95c7a011b068f1b95ddfdb5b9d0412aa60a54049
sbegaudeau <sbegaudeau>
2011-04-04 08:18:38 +0000

    [329554] Issue with i implicit and explicit i counters in for loops

--- plugins/org.eclipse.acceleo.parser/src/org/eclipse/acceleo/internal/parser/ast/CST2ASTConverterWithResolver.java
+++ plugins/org.eclipse.acceleo.parser/src/org/eclipse/acceleo/internal/parser/ast/CST2ASTConverterWithResolver.java
@@ -1166,12 +1166,16 @@ public class CST2ASTConverterWithResolver extends CST2ASTConverter {
 			if (context != null) {
 				factory.getOCL().pushContext(context);
 			}
-			// Implicit "i" variable for the iteration count
-			org.eclipse.ocl.ecore.Variable iterationCount = org.eclipse.ocl.ecore.EcoreFactory.eINSTANCE
-					.createVariable();
-			iterationCount.setName("i"); //$NON-NLS-1$
-			iterationCount.setType(factory.getOCL().getIntegerType());
-			factory.getOCL().addVariableToScope(iterationCount);
+
+			org.eclipse.ocl.ecore.Variable iterationCount = null;
+			if (oLoopVariable == null || !"i".equals(oLoopVariable.getName())) { //$NON-NLS-1$
+				// Implicit "i" variable for the iteration count
+				iterationCount = org.eclipse.ocl.ecore.EcoreFactory.eINSTANCE.createVariable();
+				iterationCount.setName("i"); //$NON-NLS-1$
+				iterationCount.setType(factory.getOCL().getIntegerType());
+				factory.getOCL().addVariableToScope(iterationCount);
+			}
+
 			try {
 				org.eclipse.acceleo.parser.cst.ModelExpression iGuard = iForBlock.getGuard();
 				org.eclipse.ocl.ecore.OCLExpression oGuard = factory.getOrCreateOCLExpression(iGuard);
@@ -1202,7 +1206,9 @@ public class CST2ASTConverterWithResolver extends CST2ASTConverter {
 				if (context != null) {
 					factory.getOCL().popContext();
 				}
-				factory.getOCL().removeVariableFromScope(iterationCount);
+				if (iterationCount != null) {
+					factory.getOCL().removeVariableFromScope(iterationCount);
+				}
 			}
 		}
 	}
