5e518206322129e7f3e96c9ecd4424039d9ee55c
sbegaudeau <sbegaudeau>
2011-04-21 11:45:32 +0000

    Fix for the mystical dependency with javax.transaction.

--- plugins/org.eclipse.acceleo.ide.ui/src/org/eclipse/acceleo/internal/ide/ui/classpath/AcceleoClasspathContainer.java
+++ plugins/org.eclipse.acceleo.ide.ui/src/org/eclipse/acceleo/internal/ide/ui/classpath/AcceleoClasspathContainer.java
@@ -16,41 +16,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.transaction.Transaction;
-
-import lpg.runtime.IAst;
-
-import org.eclipse.acceleo.common.AcceleoCommonPlugin;
-import org.eclipse.acceleo.common.internal.utils.workspace.AcceleoWorkspaceUtil;
-import org.eclipse.acceleo.engine.generation.AcceleoEngine;
 import org.eclipse.acceleo.ide.ui.AcceleoUIActivator;
 import org.eclipse.acceleo.internal.ide.ui.AcceleoUIMessages;
-import org.eclipse.acceleo.model.mtl.Template;
-import org.eclipse.acceleo.parser.AcceleoParser;
-import org.eclipse.acceleo.profiler.Profiler;
-import org.eclipse.ant.core.Task;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IContributor;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.content.IContentType;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.emf.common.EMFPlugin;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
-import org.eclipse.equinox.app.IApplication;
 import org.eclipse.jdt.core.IAccessRule;
 import org.eclipse.jdt.core.IClasspathAttribute;
 import org.eclipse.jdt.core.IClasspathContainer;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.ocl.ecore.OCL;
-import org.eclipse.ocl.helper.OCLHelper;
-import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.Bundle;
 
 /**
@@ -166,26 +143,80 @@ public class AcceleoClasspathContainer implements IClasspathContainer {
 	 */
 	public static List<IClasspathEntry> getAcceleoParserLibraryEntry() {
 		List<IClasspathEntry> libraryEntries = new ArrayList<IClasspathEntry>();
-		libraryEntries.add(computeEntryFromClass(AcceleoParser.class)); // parser
-		libraryEntries.add(computeEntryFromClass(AcceleoCommonPlugin.class)); // common
-		libraryEntries.add(computeEntryFromClass(Template.class)); // model
-		libraryEntries.add(computeEntryFromClass(EMFPlugin.class)); // emf common
-		libraryEntries.add(computeEntryFromClass(EObject.class)); // emf ecore
-		libraryEntries.add(computeEntryFromClass(EcoreResourceFactoryImpl.class)); // emf ecore xmi
-		libraryEntries.add(computeEntryFromClass(OCLHelper.class)); // ocl
-		libraryEntries.add(computeEntryFromClass(OCL.class)); // ocl ecore
-		libraryEntries.add(computeEntryFromClass(IAst.class)); // lpg runtime
 
-		libraryEntries.add(computeEntryFromClass(Platform.class)); // core.runtime
-		libraryEntries.add(computeEntryFromClass(Task.class)); // ant
-		libraryEntries.add(computeEntryFromClass(NLS.class)); // osgi
-		libraryEntries.add(computeEntryFromClass(Transaction.class)); // javax.transaction
-		libraryEntries.add(computeEntryFromClass(CoreException.class)); // equinox.common
-		libraryEntries.add(computeEntryFromClass(Job.class)); // core.job
-		libraryEntries.add(computeEntryFromClass(IContributor.class)); // equinox.registry
-		libraryEntries.add(computeEntryFromClass(InstanceScope.class)); // equinox.preferences
-		libraryEntries.add(computeEntryFromClass(IContentType.class)); // core.content type
-		libraryEntries.add(computeEntryFromClass(IApplication.class)); // equinox.app
+		IClasspathEntry classpathEntry = computeEntryFromClass("org.eclipse.acceleo.parser"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.acceleo.common"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.acceleo.model"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.emf.ecore"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.emf.ecore.xmi"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.emf.common"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.ocl.ecore"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.ocl"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("lpg.runtime.java"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+
+		classpathEntry = computeEntryFromClass("org.eclipse.core.runtime"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.osgi"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.equinox.common"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.core.jobs"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.equinox.registry"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.equinox.preferences"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.core.contenttype"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.equinox.app"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.apache.ant"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
 		return libraryEntries;
 	}
 
@@ -196,46 +227,79 @@ public class AcceleoClasspathContainer implements IClasspathContainer {
 	 */
 	public static List<IClasspathEntry> getAcceleoEngineLibraryEntry() {
 		List<IClasspathEntry> libraryEntries = new ArrayList<IClasspathEntry>();
-		libraryEntries.add(computeEntryFromClass(AcceleoEngine.class)); // engine
-		libraryEntries.add(computeEntryFromClass(AcceleoCommonPlugin.class)); // common
-		libraryEntries.add(computeEntryFromClass(Template.class)); // model
-		libraryEntries.add(computeEntryFromClass(Profiler.class)); // profiler
-		libraryEntries.add(computeEntryFromClass(EMFPlugin.class)); // emf common
-		libraryEntries.add(computeEntryFromClass(EObject.class)); // emf ecore
-		libraryEntries.add(computeEntryFromClass(EcoreResourceFactoryImpl.class)); // emf ecore xmi
-		libraryEntries.add(computeEntryFromClass(OCLHelper.class)); // ocl
-		libraryEntries.add(computeEntryFromClass(OCL.class)); // ocl ecore
-		libraryEntries.add(computeEntryFromClass(IAst.class)); // lpg runtime
+
+		IClasspathEntry classpathEntry = computeEntryFromClass("org.eclipse.acceleo.engine"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.acceleo.profiler"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.acceleo.common"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.acceleo.model"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.emf.ecore"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.emf.ecore.xmi"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.emf.common"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.ocl.ecore"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("org.eclipse.ocl"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
+		classpathEntry = computeEntryFromClass("lpg.runtime.java"); //$NON-NLS-1$
+		if (classpathEntry != null) {
+			libraryEntries.add(classpathEntry);
+		}
 		return libraryEntries;
 	}
 
 	/**
-	 * Computes the classpath entry from a given class.
+	 * Computes the classpath entry from a given bundle name.
 	 * 
-	 * @param clazz
-	 *            The class
-	 * @return The classpath entry from a given class.
+	 * @param name
+	 *            The name of the bundle
+	 * @return The classpath entry from a given bundle name.
 	 */
-	private static IClasspathEntry computeEntryFromClass(Class<?> clazz) {
+	private static IClasspathEntry computeEntryFromClass(String name) {
 		final String root = "/"; //$NON-NLS-1$
 		final String jarfile = "jar:file:"; //$NON-NLS-1$
 		final String file = "file:"; //$NON-NLS-1$
 		final String suffix = "!/"; //$NON-NLS-1$
 
 		try {
-			Bundle bundle = AcceleoWorkspaceUtil.getBundle(AcceleoEngine.class);
-			String bundlePath = FileLocator.resolve(bundle.getResource(root)).toString();
-			if (bundlePath.startsWith(jarfile)) {
-				bundlePath = bundlePath.substring(jarfile.length());
-			} else if (bundlePath.startsWith(file)) {
-				bundlePath = bundlePath.substring(file.length());
-			}
-			if (bundlePath.endsWith(suffix)) {
-				bundlePath = bundlePath.substring(0, bundlePath.length() - suffix.length());
-			}
+			Bundle bundle = Platform.getBundle(name);
+			if (bundle != null) {
+				String bundlePath = FileLocator.resolve(bundle.getResource(root)).toString();
+				if (bundlePath.startsWith(jarfile)) {
+					bundlePath = bundlePath.substring(jarfile.length());
+				} else if (bundlePath.startsWith(file)) {
+					bundlePath = bundlePath.substring(file.length());
+				}
+				if (bundlePath.endsWith(suffix)) {
+					bundlePath = bundlePath.substring(0, bundlePath.length() - suffix.length());
+				}
 
-			return JavaCore.newLibraryEntry(new Path(bundlePath), null, null, new IAccessRule[0],
-					new IClasspathAttribute[0], false);
+				return JavaCore.newLibraryEntry(new Path(bundlePath), null, null, new IAccessRule[0],
+						new IClasspathAttribute[0], false);
+			}
 		} catch (IOException e) {
 			AcceleoUIActivator.log(e, true);
 		}
