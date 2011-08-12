c2ebfb324d26880759ec76f17524f572605bed34
sbegaudeau <stephane.begaudeau@obeo.fr>
2011-08-12 16:52:32 +0200

    [353317] protected code messed up.

--- tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/unit/blocks/protectedareablock/ProtectedAreaBlockTest.java
+++ tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/unit/blocks/protectedareablock/ProtectedAreaBlockTest.java
@@ -193,4 +193,281 @@ public class ProtectedAreaBlockTest extends AbstractAcceleoTest {
 		}
 		assertEquals("There should have been a lost file created", 1, lostFiles); //$NON-NLS-1$
 	}
+
+	private void generate(String templateName) throws IOException {
+		generationRoot = new File(getGenerationRootPath("ProtectedAreaIndent")); //$NON-NLS-1$
+		referenceRoot = new File(getReferenceRootPath("ProtectedAreaIndent")); //$NON-NLS-1$
+
+		// We'll only generate for a single class
+		EObject target = null;
+		for (EObject child : inputModel.eContents()) {
+			if (child instanceof EClass) {
+				target = child;
+				break;
+			}
+		}
+		assertNotNull(target);
+		final List<EObject> templateArgs = new ArrayList<EObject>(1);
+		templateArgs.add(target);
+
+		new AcceleoService().doGenerateTemplate(module, templateName, templateArgs, generationRoot,
+				new BasicMonitor());
+
+		try {
+			compareDirectories(referenceRoot, generationRoot);
+		} catch (IOException e) {
+			fail(errorMessageForCompareDirectoriesMethod + ':' + e.getMessage());
+		}
+
+		int lostFiles = 0;
+		for (File generated : getFiles(generationRoot)) {
+			if (generated.getName().equals(templateName)) {
+				final String content = getAbsoluteFileContent(generated.getAbsolutePath());
+				assertFalse(
+						"Invalid indentation for '" + generated.getName() + "'", content.contains("ACCELEO_PROTECTED_AREA_MARKER_FIT_INDENTATION")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+				assertTrue(content.contains("Start of user code protected")); //$NON-NLS-1$
+				assertTrue(content.contains("protected block")); //$NON-NLS-1$
+				assertTrue(content.contains("End of user code")); //$NON-NLS-1$
+
+			}
+			if (generated.getName().startsWith(templateName)
+					&& generated.getName().endsWith(IAcceleoConstants.ACCELEO_LOST_FILE_EXTENSION)) {
+				lostFiles++;
+			}
+		}
+		assertEquals("A lost file has been created", 0, lostFiles); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaFileBlock() throws IOException {
+		generate("test_protected_area_file_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaFileBlockMerge() throws IOException {
+		generate("test_protected_area_file_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForBlock() throws IOException {
+		generate("test_protected_area_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForBlockMerge() throws IOException {
+		generate("test_protected_area_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetBlock() throws IOException {
+		generate("test_protected_area_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetBlockMerge() throws IOException {
+		generate("test_protected_area_let_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfBlock() throws IOException {
+		generate("test_protected_area_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfBlockMerge() throws IOException {
+		generate("test_protected_area_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForLetIfBlock() throws IOException {
+		generate("test_protected_area_for_let_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForLetIfBlockMerge() throws IOException {
+		generate("test_protected_area_for_let_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateBlock() throws IOException {
+		generate("test_protected_area_template_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateBlockMerge() throws IOException {
+		generate("test_protected_area_template_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateBlock() throws IOException {
+		generate("test_protected_area_for_template_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateBlockMerge() throws IOException {
+		generate("test_protected_area_for_template_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateBlock() throws IOException {
+		generate("test_protected_area_let_template_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateBlockMerge() throws IOException {
+		generate("test_protected_area_let_template_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateBlock() throws IOException {
+		generate("test_protected_area_if_template_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateBlockMerge() throws IOException {
+		generate("test_protected_area_if_template_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateForBlock() throws IOException {
+		generate("test_protected_area_template_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateForBlockMerge() throws IOException {
+		generate("test_protected_area_template_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateForBlock() throws IOException {
+		generate("test_protected_area_for_template_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateForBlockMerge() throws IOException {
+		generate("test_protected_area_for_template_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateForBlock() throws IOException {
+		generate("test_protected_area_let_template_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateForBlockMerge() throws IOException {
+		generate("test_protected_area_let_template_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateForBlock() throws IOException {
+		generate("test_protected_area_if_template_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateForBlockMerge() throws IOException {
+		generate("test_protected_area_if_template_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateLetBlock() throws IOException {
+		generate("test_protected_area_template_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateLetBlockMerge() throws IOException {
+		generate("test_protected_area_template_let_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateLetBlock() throws IOException {
+		generate("test_protected_area_for_template_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateTetBlockMerge() throws IOException {
+		generate("test_protected_area_for_template_let_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateLetBlock() throws IOException {
+		generate("test_protected_area_let_template_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateLetBlockMerge() throws IOException {
+		generate("test_protected_area_let_template_let_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateLetBlock() throws IOException {
+		generate("test_protected_area_if_template_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateLetBlockMerge() throws IOException {
+		generate("test_protected_area_if_template_let_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateIfBlock() throws IOException {
+		generate("test_protected_area_template_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateIfBlockMerge() throws IOException {
+		generate("test_protected_area_template_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateIfBlock() throws IOException {
+		generate("test_protected_area_for_template_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaForTemplateIfBlockMerge() throws IOException {
+		generate("test_protected_area_for_template_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateIfBlock() throws IOException {
+		generate("test_protected_area_let_template_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaLetTemplateIfBlockMerge() throws IOException {
+		generate("test_protected_area_let_template_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateIfBlock() throws IOException {
+		generate("test_protected_area_if_template_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaIfTemplateIfBlockMerge() throws IOException {
+		generate("test_protected_area_if_template_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileBlock() throws IOException {
+		generate("test_protected_area_template_file_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileForBlock() throws IOException {
+		generate("test_protected_area_template_file_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileForBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileLetBlock() throws IOException {
+		generate("test_protected_area_template_file_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileLetBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_let_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileIfBlock() throws IOException {
+		generate("test_protected_area_template_file_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileIfBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileForIfBlock() throws IOException {
+		generate("test_protected_area_template_file_for_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileForIfBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_for_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileTemplateForIfBlock() throws IOException {
+		generate("test_protected_area_template_file_template_for_if_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileTemplateForIfBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_template_for_if_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileTemplateForForBlock() throws IOException {
+		generate("test_protected_area_template_file_template_for_for_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileTemplateForForBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_template_for_for_block_merge"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileTemplateForLetBlock() throws IOException {
+		generate("test_protected_area_template_file_template_for_let_block"); //$NON-NLS-1$
+	}
+
+	public void testProtectedAreaTemplateFileTemplateForLetBlockMerge() throws IOException {
+		generate("test_protected_area_template_file_template_for_let_block_merge"); //$NON-NLS-1$
+	}
+
 }
