949cb2d50c60869b3bcd72410df7618871b7fea9
sbegaudeau <stephane.begaudeau@obeo.fr>
2011-09-21 14:33:49 +0200

    Improved the SaveExpressionAction of the Acceleo in order to save in an existing Acceleo module (that will be opened after the save) if there isn't any linked editor.

--- plugins/org.eclipse.acceleo.ide.ui/src/org/eclipse/acceleo/internal/ide/ui/interpreter/SaveExpressionAction.java
+++ plugins/org.eclipse.acceleo.ide.ui/src/org/eclipse/acceleo/internal/ide/ui/interpreter/SaveExpressionAction.java
@@ -11,14 +11,23 @@
 package org.eclipse.acceleo.internal.ide.ui.interpreter;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.acceleo.common.IAcceleoConstants;
 import org.eclipse.acceleo.ide.ui.AcceleoUIActivator;
+import org.eclipse.acceleo.ide.ui.resources.AcceleoProject;
 import org.eclipse.acceleo.internal.ide.ui.AcceleoUIMessages;
 import org.eclipse.acceleo.internal.ide.ui.editors.template.AcceleoEditor;
+import org.eclipse.acceleo.internal.parser.cst.utils.FileContent;
 import org.eclipse.acceleo.ui.interpreter.view.InterpreterView;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.ui.dialogs.WorkspaceResourceDialog;
 import org.eclipse.jface.action.Action;
@@ -27,11 +36,16 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IMenuCreator;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 /**
  * The SaveExpression action is used when the user click the save button in the interpreter view. If the
@@ -154,24 +168,69 @@ public class SaveExpressionAction extends Action implements IMenuCreator {
 	private void saveContentInNewFile(String content) {
 		String fileContent = content;
 
-		IFile iFile = WorkspaceResourceDialog.openNewFile(this.acceleoSource.getControl().getShell(),
-				AcceleoUIMessages.getString("acceleo.interpreter.new.module.path.title"), AcceleoUIMessages //$NON-NLS-1$
-						.getString("acceleo.interpreter.new.module.path"), null, null); //$NON-NLS-1$
-		if (iFile != null) {
+		// Select the Acceleo module to import
+		ViewerFilter viewerFilter = new AcceleoModulesViewerFilter();
+		List<ViewerFilter> viewerFilters = new ArrayList<ViewerFilter>();
+		viewerFilters.add(viewerFilter);
+
+		IFile[] files = WorkspaceResourceDialog.openFileSelection(this.acceleoSource.getControl().getShell(),
+				AcceleoUIMessages.getString("acceleo.interpreter.load.module.path.title"), //$NON-NLS-1$
+				AcceleoUIMessages.getString("acceleo.interpreter.load.module.path"), false, null, //$NON-NLS-1$
+				viewerFilters);
+
+		if (files != null && files.length == 1) {
 			try {
 				// Let's change the name of the new module if we still have the temporary interpreter module
 				// name
-				String fileName = iFile.getName();
-				if (fileName.endsWith('.' + IAcceleoConstants.MTL_FILE_EXTENSION)) {
-					fileName = fileName.substring(0, fileName.length() - 4);
-					fileName = "[module " + fileName + "("; //$NON-NLS-1$//$NON-NLS-2$
-					fileContent = fileContent.replace("[module temporaryInterpreterModule(", fileName); //$NON-NLS-1$
-				}
 
-				iFile.create(new ByteArrayInputStream(fileContent.getBytes()), true,
-						new NullProgressMonitor());
+				IFile iFile = files[0];
+				StringBuffer fileBufferedContent = FileContent.getFileContent(iFile.getLocation().toFile());
+
+				int indexOfTemplate = fileContent.indexOf("[template"); //$NON-NLS-1$
+				int indexOfQuery = fileContent.indexOf("[query"); //$NON-NLS-1$
+				int indexOfModuleElement = Math.min(indexOfTemplate, indexOfQuery);
+				if (indexOfTemplate == -1) {
+					indexOfModuleElement = indexOfQuery;
+				} else if (indexOfQuery == -1) {
+					indexOfModuleElement = indexOfTemplate;
+				}
+				fileContent = fileContent.substring(indexOfModuleElement);
+				fileBufferedContent.append(System.getProperty("line.separator")); //$NON-NLS-1$
+				fileBufferedContent.append(fileContent);
+
+				iFile.setContents(new ByteArrayInputStream(fileBufferedContent.toString().getBytes()), true,
+						true, new NullProgressMonitor());
+				IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), iFile);
 			} catch (CoreException e) {
 				AcceleoUIActivator.log(e, true);
+			}
+		}
+	}
+
+	/**
+	 * Adds the ".mtl" files found in the given container and its children in the list of files.
+	 * 
+	 * @param files
+	 *            The files
+	 * @param container
+	 *            The container
+	 * @throws CoreException
+	 *             In case of problems
+	 */
+	private void computeMTLFiles(List<IFile> files, IContainer container) throws CoreException {
+		if (container != null && container.isAccessible()) {
+			IResource[] children = container.members();
+			if (children != null) {
+				for (int i = 0; i < children.length; ++i) {
+					IResource resource = children[i];
+					if (resource instanceof IFile
+							&& IAcceleoConstants.MTL_FILE_EXTENSION.equals(((IFile)resource)
+									.getFileExtension())) {
+						files.add((IFile)resource);
+					} else if (resource instanceof IContainer) {
+						computeMTLFiles(files, (IContainer)resource);
+					}
+				}
 			}
 		}
 	}
@@ -226,6 +285,60 @@ public class SaveExpressionAction extends Action implements IMenuCreator {
 	protected void addActionToMenu(Menu parent, Action action) {
 		ActionContributionItem item = new ActionContributionItem(action);
 		item.fill(parent, -1);
+	}
+
+	/**
+	 * This viewer filter will only display the Acceleo projects and their folders wontaining Acceleo modules.
+	 * 
+	 * @author <a href="mailto:stephane.begaudeau@obeo.fr">Stephane Begaudeau</a>
+	 */
+	private final class AcceleoModulesViewerFilter extends ViewerFilter {
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @see org.eclipse.jface.viewers.ViewerFilter#select(org.eclipse.jface.viewers.Viewer,
+		 *      java.lang.Object, java.lang.Object)
+		 */
+		@Override
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
+			boolean result = false;
+			List<IFile> files = new ArrayList<IFile>();
+			try {
+				if (element instanceof IProject) {
+					IProject project = (IProject)element;
+					if (project.isAccessible() && project.hasNature(IAcceleoConstants.ACCELEO_NATURE_ID)) {
+						AcceleoProject acceleoProject = new AcceleoProject(project);
+						result = acceleoProject.getInputFiles().size() > 0;
+					}
+				} else if (element instanceof IContainer) {
+					IContainer container = (IContainer)element;
+					IProject project = container.getProject();
+					if (project.isAccessible() && project.hasNature(IAcceleoConstants.ACCELEO_NATURE_ID)) {
+						AcceleoProject acceleoProject = new AcceleoProject(project);
+						List<IPath> sourceFolders = acceleoProject.getSourceFolders();
+						boolean shouldVisit = false;
+						for (IPath iPath : sourceFolders) {
+							if (container.getLocation().toString().startsWith(
+									ResourcesPlugin.getWorkspace().getRoot().getFolder(iPath).getLocation()
+											.toString())) {
+								computeMTLFiles(files, container);
+								shouldVisit = files.size() > 0;
+							}
+							if (shouldVisit) {
+								break;
+							}
+						}
+						result = shouldVisit;
+					}
+				} else if (element instanceof IFile
+						&& IAcceleoConstants.MTL_FILE_EXTENSION.equals(((IFile)element).getFileExtension())) {
+					result = true;
+				}
+			} catch (CoreException e) {
+				AcceleoUIActivator.log(e, true);
+			}
+			return result;
+		}
 	}
 
 	/**
@@ -284,7 +397,7 @@ public class SaveExpressionAction extends Action implements IMenuCreator {
 				// Copy the content of the interpreter and paste it at the bottom of the linked Acceleo
 				// editor.
 				String text = acceleoSource.getTextWidget().getText();
-				if (!text.contains(AcceleoSourceViewer.TEMPLATE) && !text.contains(AcceleoSourceViewer.QUERY)) {
+				if (!text.contains(AcceleoSourceViewer.TEMPLATE)) {
 					// We will copy the content of the text in a new template
 
 					// We remove the module declaration and its imports
@@ -366,7 +479,16 @@ public class SaveExpressionAction extends Action implements IMenuCreator {
 				// Copy the content of the interpreter and paste it at the bottom of the linked Acceleo
 				// editor.
 				String text = acceleoSource.getTextWidget().getText();
-				if (!text.contains(AcceleoSourceViewer.TEMPLATE) && !text.contains(AcceleoSourceViewer.QUERY)) {
+				ISelection selection = acceleoSource.getSelection();
+				if (selection != null && selection instanceof TextSelection
+						&& ((TextSelection)selection).getLength() == 0) {
+					// No selection, let's build a whole module
+				} else if (selection != null && selection instanceof TextSelection) {
+					// Only the selection will be used
+					text = ((TextSelection)selection).getText();
+				}
+
+				if (!text.contains(AcceleoSourceViewer.QUERY)) {
 					// We will copy the content of the text in a new query
 
 					// We remove the module declaration and its imports
