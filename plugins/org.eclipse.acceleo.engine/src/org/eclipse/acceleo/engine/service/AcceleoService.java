c3bc54f4dddd376828b816f098812e58decebfcd
lgoubet <laurent.goubet@obeo.fr>
2011-10-14 14:13:13 +0200

    Allow Acceleo to launch generation on models with more than a single root

--- plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/service/AcceleoService.java
+++ plugins/org.eclipse.acceleo.engine/src/org/eclipse/acceleo/engine/service/AcceleoService.java
@@ -587,6 +587,19 @@ public final class AcceleoService {
 					generationHasOccurred = true;
 				}
 			}
+			// There is a possibility that "model" is but one of the resource's roots.
+			List<EObject> roots = new ArrayList<EObject>(model.eResource().getContents());
+			roots.remove(model);
+			for (EObject root : roots) {
+				if (argumentType.isInstance(root)) {
+					final List<Object> actualArguments = new ArrayList<Object>();
+					actualArguments.add(root);
+					actualArguments.addAll(arguments);
+					previewResult.putAll(doGenerateTemplate(template, actualArguments, generationRoot,
+							monitor));
+					generationHasOccurred = true;
+				}
+			}
 		}
 
 		// End
