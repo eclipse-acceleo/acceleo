7f829a698431ec6237a713affe2771f2fc4145a4
lgoubet <laurent.goubet@obeo.fr>
2011-10-14 13:29:38 +0200

    Creating the necessary classes to use CircularArrayDeque-based multimaps.

---
+++ plugins/org.eclipse.acceleo.common/src/org/eclipse/acceleo/common/utils/AcceleoCollections.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.common.utils;
+
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimaps;
+
+import java.util.Collection;
+
+/**
+ * This can create multimaps based on Acceleo collections.
+ * 
+ * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
+ * @since 3.2
+ */
+public final class AcceleoCollections {
+	/**
+	 * We don't need a default constructor on a factory class.
+	 */
+	private AcceleoCollections() {
+		// Hides default constructor
+	}
+
+	/**
+	 * Creates a new Multimap using {@link CircularArrayDeque}s as its backing collection.
+	 * 
+	 * @param <K>
+	 *            Type of the new map's keys.
+	 * @param <V>
+	 *            Type of the new map's values.
+	 * @return A new Multimap using {@link CircularArrayDeque}s as its backing collection.
+	 */
+	public static <K, V> ListMultimap<K, V> newCircularArrayDequeMultimap() {
+		return Multimaps.<K, V> newListMultimap(Maps.<K, Collection<V>> newHashMap(), new DequeSupplier<V>());
+	}
+}
