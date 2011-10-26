60a59963a8dc0c46b46b7f5cec423ac01acad78a
sbegaudeau <stephane.begaudeau@obeo.fr>
2011-10-26 14:13:33 +0200

    [362056] [traceability] ClassCastException with guards (unit test)

--- tests/org.eclipse.acceleo.traceability.tests/src/org/eclipse/acceleo/traceability/tests/unit/template/AcceleoTraceabilityTemplateTests.java
+++ tests/org.eclipse.acceleo.traceability.tests/src/org/eclipse/acceleo/traceability/tests/unit/template/AcceleoTraceabilityTemplateTests.java
@@ -91,6 +91,48 @@ public class AcceleoTraceabilityTemplateTests extends AbstractTraceabilityTest {
 	}
 
 	@Test
+	public void testTraceabilityTemplateGuard() {
+		AcceleoTraceabilityListener traceabilityListener = this.parseAndGenerate(
+				"data/template/templateGuard.mtl", //$NON-NLS-1$
+				"main", "data/template/model.ecore", true); //$NON-NLS-1$ //$NON-NLS-2$
+		List<GeneratedFile> generatedFiles = traceabilityListener.getGeneratedFiles();
+		assertEquals(4, generatedFiles.size());
+
+		int cpt = 1;
+		for (GeneratedFile generatedFile : generatedFiles) {
+			List<GeneratedText> generatedRegions = generatedFile.getGeneratedRegions();
+			assertEquals(1, generatedRegions.size());
+			assertEquals("templateGuard".length(), generatedFile.getLength()); //$NON-NLS-1$
+
+			List<InputElement> sourceElements = generatedFile.getSourceElements();
+			assertEquals(2, sourceElements.size()); // the class and its name
+			assertEquals("class" + cpt + ", feature='name'", sourceElements.get(0).toString()); //$NON-NLS-1$ //$NON-NLS-2$
+			assertEquals("class" + cpt, sourceElements.get(1).toString()); //$NON-NLS-1$
+			assertEquals("class" + cpt + ".txt", generatedFile.getPath()); //$NON-NLS-1$ //$NON-NLS-2$
+
+			GeneratedText generatedText = generatedRegions.get(0);
+			assertEquals(0, generatedText.getStartOffset());
+			assertEquals("templateGuard".length(), generatedText.getEndOffset()); //$NON-NLS-1$
+			ModuleElement moduleElement = generatedText.getModuleElement();
+			EObject element = moduleElement.getModuleElement();
+			assertTrue(element instanceof ASTNode);
+			assertTrue(element instanceof StringLiteralExp);
+			StringLiteralExp string = (StringLiteralExp)element;
+			assertEquals("templateGuard", string.getStringSymbol()); //$NON-NLS-1$
+			assertEquals(275, string.getStartPosition());
+			assertEquals(275 + "templateGuard".length(), string.getEndPosition()); //$NON-NLS-1$
+
+			InputElement sourceElement = generatedText.getSourceElement();
+			EObject modelElement = sourceElement.getModelElement();
+			assertTrue(modelElement instanceof EClass);
+			assertEquals("class" + cpt, ((EClass)modelElement).getName()); //$NON-NLS-1$
+			assertEquals("/plugin/org.eclipse.acceleo.traceability.tests/data/template/model.ecore", //$NON-NLS-1$
+					modelElement.eResource().getURI().path());
+			cpt++;
+		}
+	}
+
+	@Test
 	public void testTraceabilityTemplateMultipleParameters() {
 		AcceleoTraceabilityListener traceabilityListener = this.parseAndGenerate(
 				"data/template/templateMultipleParameters.mtl", //$NON-NLS-1$
