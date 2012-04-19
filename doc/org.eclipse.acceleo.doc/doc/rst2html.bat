c0ffbd6c741d1dd4a1dfcd64051b3a066324632b
sbegaudeau <stephane.begaudeau@obeo.fr>
2012-04-19 11:57:39 +0200

    First version of the new documentation.

--- doc/org.eclipse.acceleo.doc/doc/rst2html.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-FOR %%f IN (rst\*.rst) DO (
-	FOR %%h IN (html\%%~nf.html) DO (
-		IF %%~tf GTR %%~th (
-			C:\python26\python.exe "C:\Program Files\docutils\tools\rst2html.py" %%f html\%%~nf.html --link-stylesheet --stylesheet-path=style/acceleo.css
-		)
-	)
-)
--- doc/org.eclipse.acceleo.doc/doc/rst2html4SBE.bat
+++
@@ -1,6 +0,0 @@
-@ECHO ON
-FOR %%f IN (rst\*.rst) DO (
-	FOR %%h IN (html\%%~nf.html) DO (
-		C:\python27\python.exe "C:\Program Files (x86)\docutilsv2\tools\rst2html.py" %%f html\%%~nf.html --link-stylesheet --stylesheet-path=style/acceleo.css
-	)
-)
