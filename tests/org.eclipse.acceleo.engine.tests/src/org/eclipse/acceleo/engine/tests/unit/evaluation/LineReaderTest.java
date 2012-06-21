4732d8cd5dfd67fb33b8246b99f5fcb658b4c6fc
sbegaudeau <stephane.begaudeau@obeo.fr>
2012-06-21 10:47:58 +0200

    Line reader unit test

---
+++ tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/unit/evaluation/LineReaderTest.java
@@ -0,0 +1,177 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.engine.tests.unit.evaluation;
+
+import com.google.common.collect.Lists;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+
+import org.eclipse.acceleo.engine.event.IAcceleoTextGenerationListener;
+import org.eclipse.acceleo.engine.internal.evaluation.AcceleoEvaluationContext;
+import org.eclipse.emf.ecore.EClassifier;
+
+/**
+ * Unit tests classe to test the line reader of the evaluation context.
+ * 
+ * @author <a href="mailto:stephane.begaudeau@obeo.fr">Stephane Begaudeau</a>
+ */
+public class LineReaderTest extends TestCase {
+	private int readLine(String str) {
+		AcceleoEvaluationContext<EClassifier> evaluationContext = new AcceleoEvaluationContext<EClassifier>(
+				null, Lists.<IAcceleoTextGenerationListener> newArrayList(), null, null);
+		AcceleoEvaluationContext<EClassifier>.LineReader reader = evaluationContext.new LineReader(
+				new StringReader(str));
+		String line;
+		int i = 0;
+		try {
+			line = reader.readLine();
+			if (line != null) {
+				i = i + 1;
+			}
+			while (line != null) {
+				line = reader.readLine();
+				if (line != null) {
+					i = i + 1;
+				}
+			}
+
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		return i;
+	}
+
+	public void testEmptyStringReadLine() {
+		assertEquals(0, readLine("")); //$NON-NLS-1$
+	}
+
+	public void testOneLineReadLine() {
+		assertEquals(1, readLine("test")); //$NON-NLS-1$
+	}
+
+	public void testOneBigLineReadLine() {
+		String str = "0123456789"; //$NON-NLS-1$
+		String buffer = str;
+		for (int i = 0; i < 818; i++) {
+			buffer = buffer + str;
+		}
+		buffer = buffer + "012"; //$NON-NLS-1$
+		assertEquals(1, readLine(buffer));
+	}
+
+	public void testTwoLinesUnixReadLine() {
+		assertEquals(2, readLine("test\ntest")); //$NON-NLS-1$
+	}
+
+	public void testTwoBigLinesMacOsClassicReadLine() {
+		String str = "0123456789"; //$NON-NLS-1$
+		String buffer = str;
+		for (int i = 0; i < 818; i++) {
+			buffer = buffer + str;
+		}
+
+		// Index of \r = 8192
+		String finalBuffer = buffer + "01\r2"; //$NON-NLS-1$
+		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+
+		// Index of \r = 8190
+		finalBuffer = buffer + "\r01"; //$NON-NLS-1$
+		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+
+		// Index of \r = 8191
+		finalBuffer = buffer + "0\r1"; //$NON-NLS-1$
+		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+	}
+
+	public void testTwoBigLinesDosReadLine() {
+		String str = "0123456789"; //$NON-NLS-1$
+		String buffer = str;
+		for (int i = 0; i < 818; i++) {
+			buffer = buffer + str;
+		}
+
+		// Index of \r = 8192
+		String finalBuffer = buffer + "01\r\n2"; //$NON-NLS-1$
+		System.out.println("Index of \\r\\n: " + finalBuffer.indexOf("\r\n")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+
+		// Index of \r = 8190
+		finalBuffer = buffer + "\r\n1"; //$NON-NLS-1$
+		System.out.println("Index of \\r\\n: " + finalBuffer.indexOf("\r\n")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+
+		// Index of \r = 8191
+		finalBuffer = buffer + "0\r\n"; //$NON-NLS-1$
+		System.out.println("Index of \\r\\n: " + finalBuffer.indexOf("\r\n")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+	}
+
+	public void testTwoBigLinesUnixReadLine() {
+		String str = "0123456789"; //$NON-NLS-1$
+		String buffer = str;
+		for (int i = 0; i < 818; i++) {
+			buffer = buffer + str;
+		}
+
+		// Index of \r = 8192
+		String finalBuffer = buffer + "01\n2"; //$NON-NLS-1$
+		System.out.println("Index of \\n: " + finalBuffer.indexOf("\n")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+
+		// Index of \r = 8190
+		finalBuffer = buffer + "\n01"; //$NON-NLS-1$
+		System.out.println("Index of \\n: " + finalBuffer.indexOf("\n")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+
+		// Index of \r = 8191
+		finalBuffer = buffer + "0\n1"; //$NON-NLS-1$
+		System.out.println("Index of \\n: " + finalBuffer.indexOf("\n")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(2, readLine(finalBuffer));
+	}
+
+	public void testTwoLinesDosReadLine() {
+		assertEquals(2, readLine("test\r\ntest")); //$NON-NLS-1$
+	}
+
+	public void testTwoLinesMacOsClassicReadLine() {
+		assertEquals(2, readLine("test\rtest")); //$NON-NLS-1$
+	}
+
+	public void testTwoLinesUnixEmptyReadLine() {
+		assertEquals(2, readLine("test\ntest\n")); //$NON-NLS-1$
+	}
+
+	public void testTwoLinesDosEmptyReadLine() {
+		assertEquals(2, readLine("test\r\ntest\r\n")); //$NON-NLS-1$
+	}
+
+	public void testTwoLinesMacOsClassicEmptyReadLine() {
+		assertEquals(2, readLine("test\rtest\r")); //$NON-NLS-1$
+	}
+
+	public void testThreeLinesUnixReadLine() {
+		assertEquals(3, readLine("test\ntest\na")); //$NON-NLS-1$
+	}
+
+	public void testThreeLinesDosReadLine() {
+		assertEquals(3, readLine("test\r\ntest\r\na")); //$NON-NLS-1$
+	}
+
+	public void testThreeLinesMacOsClassicReadLine() {
+		assertEquals(3, readLine("test\rtest\ra")); //$NON-NLS-1$
+	}
+}
