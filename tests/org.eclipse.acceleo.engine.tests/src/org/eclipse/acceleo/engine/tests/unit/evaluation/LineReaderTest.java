3b8b37d1b274f9b8f12a71ed175bd37927169d53
sbegaudeau <stephane.begaudeau@obeo.fr>
2012-08-06 16:37:38 +0200

    Improved the fix for the index 8191/8192 and updated the unit tests that go with it.

--- tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/unit/evaluation/LineReaderTest.java
+++ tests/org.eclipse.acceleo.engine.tests/src/org/eclipse/acceleo/engine/tests/unit/evaluation/LineReaderTest.java
@@ -14,6 +14,7 @@ import com.google.common.collect.Lists;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.List;
 
 import junit.framework.TestCase;
 
@@ -27,7 +28,7 @@ import org.eclipse.emf.ecore.EClassifier;
  * @author <a href="mailto:stephane.begaudeau@obeo.fr">Stephane Begaudeau</a>
  */
 public class LineReaderTest extends TestCase {
-	private int readLine(String str) {
+	private int readLine(String str, List<String> lines) {
 		AcceleoEvaluationContext<EClassifier> evaluationContext = new AcceleoEvaluationContext<EClassifier>(
 				null, Lists.<IAcceleoTextGenerationListener> newArrayList(), null, null);
 		AcceleoEvaluationContext<EClassifier>.LineReader reader = evaluationContext.new LineReader(
@@ -37,11 +38,15 @@ public class LineReaderTest extends TestCase {
 		try {
 			line = reader.readLine();
 			if (line != null) {
+				assertEquals(lines.get(0), line);
 				i = i + 1;
 			}
+			int count = 1;
 			while (line != null) {
 				line = reader.readLine();
 				if (line != null) {
+					assertEquals(lines.get(count), line);
+					count++;
 					i = i + 1;
 				}
 			}
@@ -53,11 +58,11 @@ public class LineReaderTest extends TestCase {
 	}
 
 	public void testEmptyStringReadLine() {
-		assertEquals(0, readLine("")); //$NON-NLS-1$
+		assertEquals(0, readLine("", Lists.<String> newArrayList())); //$NON-NLS-1$
 	}
 
 	public void testOneLineReadLine() {
-		assertEquals(1, readLine("test")); //$NON-NLS-1$
+		assertEquals(1, readLine("test", Lists.newArrayList("test"))); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	public void testOneBigLineReadLine() {
@@ -67,11 +72,11 @@ public class LineReaderTest extends TestCase {
 			buffer = buffer + str;
 		}
 		buffer = buffer + "012"; //$NON-NLS-1$
-		assertEquals(1, readLine(buffer));
+		assertEquals(1, readLine(buffer, Lists.newArrayList(buffer)));
 	}
 
 	public void testTwoLinesUnixReadLine() {
-		assertEquals(2, readLine("test\ntest")); //$NON-NLS-1$
+		assertEquals(2, readLine("test\ntest", Lists.newArrayList("test", "test"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 	public void testTwoBigLinesMacOsClassicReadLine() {
@@ -84,17 +89,24 @@ public class LineReaderTest extends TestCase {
 		// Index of \r = 8192
 		String finalBuffer = buffer + "01\r2"; //$NON-NLS-1$
 		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer + "01", "2"))); //$NON-NLS-1$ //$NON-NLS-2$
 
 		// Index of \r = 8190
 		finalBuffer = buffer + "\r01"; //$NON-NLS-1$
 		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer, "01"))); //$NON-NLS-1$
 
 		// Index of \r = 8191
 		finalBuffer = buffer + "0\r1"; //$NON-NLS-1$
 		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(1, readLine(finalBuffer, Lists.newArrayList(buffer + "0", "1"))); //$NON-NLS-1$ //$NON-NLS-2$
+
+		// Index of \r = 8191
+		finalBuffer = buffer + "0\r1"; //$NON-NLS-1$
+		finalBuffer = "01234\r5678" + finalBuffer; //$NON-NLS-1$
+
+		System.out.println("Index of \\r: " + finalBuffer.indexOf("\r")); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(3, readLine(finalBuffer, Lists.newArrayList("01234", "5678" + buffer + "0", "1"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	}
 
 	public void testTwoBigLinesDosReadLine() {
@@ -107,17 +119,17 @@ public class LineReaderTest extends TestCase {
 		// Index of \r = 8192
 		String finalBuffer = buffer + "01\r\n2"; //$NON-NLS-1$
 		System.out.println("Index of \\r\\n: " + finalBuffer.indexOf("\r\n")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer + "01", "2"))); //$NON-NLS-1$ //$NON-NLS-2$
 
 		// Index of \r = 8190
 		finalBuffer = buffer + "\r\n1"; //$NON-NLS-1$
 		System.out.println("Index of \\r\\n: " + finalBuffer.indexOf("\r\n")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer, "1"))); //$NON-NLS-1$
 
 		// Index of \r = 8191
-		finalBuffer = buffer + "0\r\n"; //$NON-NLS-1$
+		finalBuffer = buffer + "0\r\n1"; //$NON-NLS-1$
 		System.out.println("Index of \\r\\n: " + finalBuffer.indexOf("\r\n")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer + "0", "1"))); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	public void testTwoBigLinesUnixReadLine() {
@@ -130,48 +142,48 @@ public class LineReaderTest extends TestCase {
 		// Index of \r = 8192
 		String finalBuffer = buffer + "01\n2"; //$NON-NLS-1$
 		System.out.println("Index of \\n: " + finalBuffer.indexOf("\n")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer + "01", "2"))); //$NON-NLS-1$ //$NON-NLS-2$
 
 		// Index of \r = 8190
 		finalBuffer = buffer + "\n01"; //$NON-NLS-1$
 		System.out.println("Index of \\n: " + finalBuffer.indexOf("\n")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer, "01"))); //$NON-NLS-1$
 
 		// Index of \r = 8191
 		finalBuffer = buffer + "0\n1"; //$NON-NLS-1$
 		System.out.println("Index of \\n: " + finalBuffer.indexOf("\n")); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(2, readLine(finalBuffer));
+		assertEquals(2, readLine(finalBuffer, Lists.newArrayList(buffer + "0", "1"))); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	public void testTwoLinesDosReadLine() {
-		assertEquals(2, readLine("test\r\ntest")); //$NON-NLS-1$
+		assertEquals(2, readLine("test\r\ntest", Lists.newArrayList("test", "test"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 	public void testTwoLinesMacOsClassicReadLine() {
-		assertEquals(2, readLine("test\rtest")); //$NON-NLS-1$
+		assertEquals(2, readLine("test\rtest", Lists.newArrayList("test", "test"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 	public void testTwoLinesUnixEmptyReadLine() {
-		assertEquals(2, readLine("test\ntest\n")); //$NON-NLS-1$
+		assertEquals(2, readLine("test\ntest\n", Lists.newArrayList("test", "test"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 	public void testTwoLinesDosEmptyReadLine() {
-		assertEquals(2, readLine("test\r\ntest\r\n")); //$NON-NLS-1$
+		assertEquals(2, readLine("test\r\ntest\r\n", Lists.newArrayList("test", "test"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 	public void testTwoLinesMacOsClassicEmptyReadLine() {
-		assertEquals(2, readLine("test\rtest\r")); //$NON-NLS-1$
+		assertEquals(2, readLine("test\rtest\r", Lists.newArrayList("test", "test"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 	public void testThreeLinesUnixReadLine() {
-		assertEquals(3, readLine("test\ntest\na")); //$NON-NLS-1$
+		assertEquals(3, readLine("test\ntest\na", Lists.newArrayList("test", "test", "a"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	}
 
 	public void testThreeLinesDosReadLine() {
-		assertEquals(3, readLine("test\r\ntest\r\na")); //$NON-NLS-1$
+		assertEquals(3, readLine("test\r\ntest\r\na", Lists.newArrayList("test", "test", "a"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	}
 
 	public void testThreeLinesMacOsClassicReadLine() {
-		assertEquals(3, readLine("test\rtest\ra")); //$NON-NLS-1$
+		assertEquals(3, readLine("test\rtest\ra", Lists.newArrayList("test", "test", "a"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	}
 }
