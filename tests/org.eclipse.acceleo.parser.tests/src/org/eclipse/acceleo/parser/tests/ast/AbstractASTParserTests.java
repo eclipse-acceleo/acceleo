61878021b5991d235c5fb831e872924413284382
sbegaudeau <stephane.begaudeau@obeo.fr>
2013-12-20 15:52:59 +0100

    Remove useless logging.

--- tests/org.eclipse.acceleo.parser.tests/src/org/eclipse/acceleo/parser/tests/ast/AbstractASTParserTests.java
+++ tests/org.eclipse.acceleo.parser.tests/src/org/eclipse/acceleo/parser/tests/ast/AbstractASTParserTests.java
@@ -184,7 +184,6 @@ public abstract class AbstractASTParserTests {
 			IFolder folder = currentContainer.getFolder(new Path(string));
 			if (!folder.exists()) {
 				try {
-					System.out.println("folder: " + folder.getLocation());
 					folder.create(true, true, new NullProgressMonitor());
 				} catch (CoreException e) {
 					fail(e.getMessage());
@@ -194,13 +193,11 @@ public abstract class AbstractASTParserTests {
 			currentContainer = folder;
 		}
 
-		System.out.println("Current container: " + currentContainer.getLocation());
 		IFile file = currentContainer.getFile(new Path(name));
 		if (!file.exists()) {
 			try {
 				ByteArrayInputStream source = new ByteArrayInputStream(content.getBytes("UTF8")); //$NON-NLS-1$
 				file.create(source, true, new NullProgressMonitor());
-				System.out.println("File created: " + file.getLocation());
 			} catch (UnsupportedEncodingException e) {
 				fail(e.getMessage());
 				return null;
