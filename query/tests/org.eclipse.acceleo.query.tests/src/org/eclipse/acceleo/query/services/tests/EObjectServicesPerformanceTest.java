565d59f92a2866e4464bd8b4c91a6c4821d63fe6
cbrun <cedric.brun@obeo.fr>
2016-02-05 14:20:31 +0100

    [487066] Add test benchmarking AQL's eAllContents/eContents versus EMF
    Bug: 487066
    Change-Id: I4045f4c2a622c4a031ef84289d39832bbd15cd67

---
+++ query/tests/org.eclipse.acceleo.query.tests/src/org/eclipse/acceleo/query/services/tests/EObjectServicesPerformanceTest.java
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.acceleo.query.services.tests;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.acceleo.query.runtime.IQueryEnvironment;
+import org.eclipse.acceleo.query.runtime.Query;
+import org.eclipse.acceleo.query.services.EObjectServices;
+import org.eclipse.acceleo.query.tests.Setup;
+import org.eclipse.acceleo.query.tests.UnitTestModels;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Benchmarks for EObject Services
+ * 
+ * @author cbrun
+ */
+public class EObjectServicesPerformanceTest {
+
+	/**
+	 * 
+	 */
+	private static final int NB_ITERATIONS = 400;
+
+	public static Resource reverseEcoreModel;
+
+	private IQueryEnvironment queryEnvironment;
+
+	private EObjectServices eObjectService;
+
+	@BeforeClass
+	public static void loadModel() throws Exception {
+		reverseEcoreModel = new UnitTestModels(Setup.createSetupForCurrentEnvironment()).reverseEcore();
+	}
+
+	@Before
+	public void before() throws Exception {
+		this.queryEnvironment = Query.newEnvironmentWithDefaultServices(null);
+		this.queryEnvironment.registerEPackage(EcorePackage.eINSTANCE);
+		this.eObjectService = new EObjectServices(queryEnvironment, null, null);
+	}
+
+	@Test
+	public void eAllContentsBenchmark() {
+		Stopwatch emf = Stopwatch.createUnstarted();
+		Stopwatch aql = Stopwatch.createUnstarted();
+		for (int i = 0; i < NB_ITERATIONS; i++) {
+			for (EObject root : reverseEcoreModel.getContents()) {
+				emf.start();
+				List<EPackage> ePackagesEMF = Lists.newArrayList(Iterators.filter(root.eAllContents(),
+						EPackage.class));
+				emf.stop();
+				aql.start();
+				List<EObject> ePackagesAQL = eObjectService.eAllContents(root, EcorePackage.eINSTANCE
+						.getEPackage());
+				aql.stop();
+
+				assertEquals(ePackagesEMF, ePackagesAQL);
+				for (EPackage ePackage : ePackagesEMF) {
+					emf.start();
+					List<EClass> eclassesEMF = Lists.newArrayList(Iterators.filter(ePackage.eAllContents(),
+							EClass.class));
+					emf.stop();
+					aql.start();
+					final List<EObject> eClassesAQL = eObjectService.eAllContents(ePackage,
+							EcorePackage.eINSTANCE.getEClass());
+					aql.stop();
+					assertEquals(eclassesEMF, eClassesAQL);
+				}
+			}
+		}
+		System.out.println("PERFO: eAllContents(Type)  :  AQL " + aql.elapsed(TimeUnit.MILLISECONDS)
+				+ "ms /  EMF : " + emf.elapsed(TimeUnit.MILLISECONDS) + "ms");
+		assertTrue("The AQL implementation is supposed to be faster than the EMF one.", aql
+				.elapsed(TimeUnit.MILLISECONDS) < emf.elapsed(TimeUnit.MILLISECONDS));
+	}
+
+	@Test
+	public void eContentsBenchmark() {
+		Stopwatch emf = Stopwatch.createUnstarted();
+		Stopwatch aql = Stopwatch.createUnstarted();
+		for (int i = 0; i < NB_ITERATIONS; i++) {
+			for (EObject root : reverseEcoreModel.getContents()) {
+				emf.start();
+				List<EPackage> ePackagesEMF = Lists.newArrayList(Iterables.filter(root.eContents(),
+						EPackage.class));
+				emf.stop();
+				aql.start();
+				List<EObject> ePackagesAQL = eObjectService.eContents(root, EcorePackage.eINSTANCE
+						.getEPackage());
+				aql.stop();
+
+				assertEquals(ePackagesEMF, ePackagesAQL);
+				for (EPackage ePackage : ePackagesEMF) {
+					emf.start();
+					List<EClass> eclassesEMF = Lists.newArrayList(Iterables.filter(ePackage.eContents(),
+							EClass.class));
+					emf.stop();
+					aql.start();
+					final List<EObject> eClassesAQL = eObjectService.eContents(ePackage,
+							EcorePackage.eINSTANCE.getEClass());
+					aql.stop();
+					assertEquals(eclassesEMF, eClassesAQL);
+				}
+			}
+		}
+		System.out.println("PERFO: eContents(Type)  :  AQL " + aql.elapsed(TimeUnit.MILLISECONDS)
+				+ "ms /  EMF : " + emf.elapsed(TimeUnit.MILLISECONDS) + "ms");
+		assertTrue("The AQL implementation is supposed to be faster than the EMF one.", aql
+				.elapsed(TimeUnit.MILLISECONDS) < emf.elapsed(TimeUnit.MILLISECONDS));
+	}
+
+}
--- query/tests/org.eclipse.acceleo.query.tests/src/org/eclipse/acceleo/query/tests/AllTests.java
+++ query/tests/org.eclipse.acceleo.query.tests/src/org/eclipse/acceleo/query/tests/AllTests.java
@@ -34,6 +34,7 @@ import org.eclipse.acceleo.query.services.tests.CollectionServicesTest;
 import org.eclipse.acceleo.query.services.tests.CollectionServicesValidationTest;
 import org.eclipse.acceleo.query.services.tests.ComparableServicesTest;
 import org.eclipse.acceleo.query.services.tests.ComparableServicesValidationTest;
+import org.eclipse.acceleo.query.services.tests.EObjectServicesPerformanceTest;
 import org.eclipse.acceleo.query.services.tests.EObjectServicesTest;
 import org.eclipse.acceleo.query.services.tests.EObjectServicesValidationTest;
 import org.eclipse.acceleo.query.services.tests.NumberServicesTest;
@@ -90,7 +91,7 @@ import org.junit.runners.Suite.SuiteClasses;
 		EEnumLiteralCompletionProposalTests.class, VariableCompletionProposalTests.class,
 		EClassifierCompletionProposalTests.class, ValidationInferrenceTest.class, TypeTests.class,
 		QueryEnvironmentTests.class, EPackageProviderTests.class, CompletionCheck.class,
-		FilterCamelCaseTest.class })
+		FilterCamelCaseTest.class, EObjectServicesPerformanceTest.class })
 public class AllTests {
 
 }
