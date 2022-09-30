c8cf5fd80058a0d4a868a91e84e57aebcd6b54dc
lgoubet <laurent.goubet@obeo.fr>
2022-09-30 16:37:54 +0200

    JenkinsFile and scripts for Acceleo build
    cherry-picked from 71a2ba

---
+++ releng/org.eclipse.acceleo.releng/publish-nightly.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# ====================================================================
+# Copyright (c) 2021 Obeo
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0
+#
+# Contributors:
+#    Obeo - initial API and implementation
+# ====================================================================
+
+# Exit on error
+set -e
+
+# The SSH account to use
+export SSH_ACCOUNT="genie.acceleo@projects-storage.eclipse.org"
+
+NIGHTLIES_FOLDER="/home/data/httpd/download.eclipse.org/acceleo/updates/nightly"
+GROUP="modeling.acceleo"
+
+UPDATE_FOLDER=${WORKSPACE}/releng/org.eclipse.acceleo-updatesite/target
+UPDATE_ZIP="$(ls ${UPDATE_FOLDER}/org.eclipse.acceleo-*.zip | sort -V | tail -n1)"
+
+ZIP_NAME=$(echo ${UPDATE_ZIP} | sed 's/.*\(org.eclipse.acceleo-.*.zip\)$/\1/')
+QUALIFIER=$(echo ${UPDATE_ZIP} | sed 's/.*org.eclipse.acceleo-\(.*\).zip$/\1/')
+
+P2_TIMESTAMP=$(date +"%s000")
+
+ssh "${SSH_ACCOUNT}" mkdir -p ${NIGHTLIES_FOLDER}/${QUALIFIER}
+scp -rp ${UPDATE_ZIP} "${SSH_ACCOUNT}:${NIGHTLIES_FOLDER}/${QUALIFIER}"
+
+# Unzip the repository and change the permissions
+ssh "${SSH_ACCOUNT}" -T <<EOF
+  pushd ${NIGHTLIES_FOLDER}/${QUALIFIER}
+    unzip "${ZIP_NAME}"
+    rm ${ZIP_NAME}
+  popd
+  
+  # make sure permissions are for the acceleo group
+  chgrp -R ${GROUP} ${NIGHTLIES_FOLDER}/${QUALIFIER}
+  chmod -R g+w ${NIGHTLIES_FOLDER}/${QUALIFIER}
+
+  pushd ${NIGHTLIES_FOLDER}/latest
+    rm -r *
+    cp -r ../${QUALIFIER}/* .
+  popd
+EOF
