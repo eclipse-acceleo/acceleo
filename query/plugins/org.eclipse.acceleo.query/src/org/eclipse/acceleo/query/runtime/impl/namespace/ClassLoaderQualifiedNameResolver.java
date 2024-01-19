28a6e70f20a8473132e7775423c5e9a277dbb041
Yvan Lussaud <yvan.lussaud@obeo.fr>
2024-01-19 15:06:15 +0100

    Fixed exception and error catching from class loading.

--- query/plugins/org.eclipse.acceleo.query/src/org/eclipse/acceleo/query/runtime/impl/namespace/ClassLoaderQualifiedNameResolver.java
+++ query/plugins/org.eclipse.acceleo.query/src/org/eclipse/acceleo/query/runtime/impl/namespace/ClassLoaderQualifiedNameResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2023 Obeo.
+ * Copyright (c) 2020, 2024 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -468,7 +468,7 @@ public class ClassLoaderQualifiedNameResolver implements IQualifiedNameResolver 
 
 		try {
 			res = classLoader.loadClass(qualifiedName.replace(qualifierSeparator, DOT));
-		} catch (ClassNotFoundException e) {
+		} catch (Exception | Error e) {
 			res = null;
 		}
 
