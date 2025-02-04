c79dde7c52598888f480fbbe8639df6d63d01888
Yvan Lussaud <yvan.lussaud@obeo.fr>
2025-02-04 16:05:34 +0100

    Moved AQL documentation from Textile to Asciidoc.

--- query/plugins/org.eclipse.acceleo.query.doc/src/org/eclipse/acceleo/query/doc/internal/AQLHelpContentUtils.java
+++ query/plugins/org.eclipse.acceleo.query.doc/src/org/eclipse/acceleo/query/doc/internal/AQLHelpContentUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2024 Obeo.
+ * Copyright (c) 2015, 2025 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -137,65 +137,6 @@ public final class AQLHelpContentUtils {
 	}
 
 	/**
-	 * Old method signature generator.
-	 * 
-	 * @author <a href="mailto:yvan.lussaud@obeo.fr">Yvan Lussaud</a>
-	 */
-	private static final class MethodSignatureOldGenerator implements Function<Method, StringBuffer> {
-		@Override
-		public StringBuffer apply(Method method) {
-			StringBuffer result = new StringBuffer();
-			result.append(method.getName()).append('(');
-			boolean first = true;
-
-			List<String> parameterNames = new ArrayList<String>();
-			Documentation documentation = method.getAnnotation(Documentation.class);
-			Param[] params = documentation.params();
-			if (params != null) {
-				for (Param param : params) {
-					parameterNames.add(param.name());
-				}
-			}
-
-			Class<?>[] parameterTypes = method.getParameterTypes();
-			for (int i = 0; i < parameterTypes.length; i = i + 1) {
-				Object argType = parameterTypes[i];
-				if (!first) {
-					result.append(", ");
-				} else {
-					first = false;
-				}
-
-				if (parameterNames.size() >= i + 1) {
-					String paramName = parameterNames.get(i);
-					if (paramName.trim().length() > 0) {
-						result.append(paramName);
-						result.append(": ");
-					}
-				}
-
-				if (argType instanceof Class<?>) {
-					result.append(((Class<?>)argType).getCanonicalName());
-				} else if (argType instanceof EClass) {
-					result.append("EClass=" + ((EClass)argType).getName());
-				} else {
-					result.append("Object=" + argType.toString());
-				}
-			}
-			result.append(')');
-
-			Class<?> returnType = method.getReturnType();
-			if (Void.class.equals(returnType)) {
-				result.append(" = void");
-			} else {
-				result.append(" = ");
-				result.append(returnType.getSimpleName());
-			}
-			return result;
-		}
-	}
-
-	/**
 	 * The prefix to use to create internal links.
 	 */
 	public static final String AQL_HREF_PREFIX = "aql_service_";
@@ -208,208 +149,13 @@ public final class AQLHelpContentUtils {
 	/**
 	 * The line separator.
 	 */
-	private static final String LS = System.getProperty("line.separator");
-
-	/**
-	 * Old method signature generator.
-	 */
-	private static final Function<Method, StringBuffer> METHODE_SIGNATURE_OLD_GENERATOR = new MethodSignatureOldGenerator();
+	public static final String LS = System.getProperty("line.separator");
 
 	/**
 	 * The constructor.
 	 */
 	private AQLHelpContentUtils() {
 		// Prevent instantiation
-	}
-
-	/**
-	 * Produces the HTML page.
-	 * 
-	 * @param head
-	 *            The content of the head
-	 * @param body
-	 *            The content of the body
-	 * @return The HTML content
-	 */
-	public static StringBuffer html(StringBuffer head, StringBuffer body) {
-		// @formatter:off
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("<!DOCTYPE html>").append(LS);
-		buffer.append("<html lang=\"en\">").append(LS);
-		buffer.append("<!--").append(LS);
-		buffer.append("/********************************************************************************").append(LS);
-		buffer.append("** Copyright (c) 2015 Obeo.").append(LS);
-		buffer.append("** All rights reserved. This program and the accompanying materials").append(LS);
-		buffer.append("** are made available under the terms of the Eclipse Public License v2.0").append(LS);
-		buffer.append("** which accompanies this distribution, and is available at").append(LS);
-		buffer.append("** http://www.eclipse.org/legal/epl-v20.html").append(LS);
-		buffer.append("**").append(LS);
-		buffer.append("** Contributors:").append(LS);
-		buffer.append("**    Stephane Begaudeau (Obeo) - initial API and implementation").append(LS);
-		buffer.append("*********************************************************************************/").append(LS);
-		buffer.append("-->").append(LS);
-
-		buffer.append(head).append(LS);
-		buffer.append(body).append(LS);
-
-		buffer.append("</html>").append(LS);
-		// @formatter:on
-		return buffer;
-	}
-
-	/**
-	 * Produces the head element.
-	 * 
-	 * @return The content of the head element
-	 */
-	public static StringBuffer head() {
-		// @formatter:off
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("  <head>").append(LS);
-		buffer.append("    <meta charset=\"utf-8\">").append(LS);
-		buffer.append("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">").append(LS);
-		buffer.append("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">").append(LS);
-		buffer.append("    <meta name=\"description\" content=\"\">").append(LS);
-		buffer.append("    <meta name=\"author\" content=\"St&eacute;phane B&eacute;gaudeau\">").append(LS);
-
-		buffer.append("    <!-- IE6-8 support of HTML elements -->").append(LS);
-		buffer.append("    <!--[if lt IE 9]>").append(LS);
-		buffer.append("      <script src=\"http://html5shim.googlecode.com/svn/trunk/html5.js\"></script>").append(LS);
-		buffer.append("    <![endif]-->").append(LS);
-
-		buffer.append("    <link href=\"../assets/css/bootstrap.css\" rel=\"stylesheet\">").append(LS);
-		buffer.append("    <link href=\"../assets/css/docs.css\" rel=\"stylesheet\">").append(LS);
-
-		buffer.append("    <title>Acceleo</title>").append(LS);
-		buffer.append("  </head>").append(LS);
-		// @formatter:on
-		return buffer;
-	}
-
-	/**
-	 * Produces the content of the body.
-	 * 
-	 * @param header
-	 *            The header
-	 * @param sections
-	 *            The sections to display
-	 * @return The content of the body
-	 */
-	public static StringBuffer body(StringBuffer header, List<StringBuffer> sections) {
-		// @formatter:off
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("  <body>").append(LS);
-		buffer.append("    <div class=\"container\">").append(LS);
-
-		buffer.append(header).append(LS);
-
-		for (StringBuffer section : sections) {
-			buffer.append(section).append(LS);
-		}
-
-		buffer.append("    </div>").append(LS);
-		buffer.append("  </body>").append(LS);
-		// @formatter:on
-		return buffer;
-	}
-
-	/**
-	 * Produces the content of the header.
-	 * 
-	 * @param isMainPage
-	 *            Indicates if we are computing the header of the main page
-	 * @return The content of the header element
-	 */
-	public static StringBuffer header(boolean isMainPage) {
-		StringBuffer buffer = new StringBuffer();
-		// @formatter:off
-		buffer.append("   <header class=\"jumbotron subhead\" id=\"overview\">").append(LS);
-		if (!isMainPage) {			
-			buffer.append("    <h1>Acceleo Query Language Documentation</h1>").append(LS);
-		}
-		buffer.append("    <!--<div class=\"subnav\">").append(LS);
-		buffer.append("       <ul class=\"nav nav-pills\">").append(LS);
-		buffer.append("        <li><a href=\"#introduction\">Introduction</a></li>").append(LS);
-		buffer.append("        <li><a href=\"#language\">Language</a></li>").append(LS);
-		buffer.append("        <li><a href=\"#operations\">Operations</a></li>").append(LS);
-		buffer.append("        <li><a href=\"#standalone\">Stand Alone</a></li>").append(LS);
-		buffer.append("        <li><a href=\"#migration\">Migration</a></li>").append(LS);
-		buffer.append("        <li><a href=\"#textproductionrules\">Text Production Rules</a></li>").append(LS);
-		buffer.append("        <li><a href=\"#onlineresources\">Online Resources</a></li>").append(LS);
-		buffer.append("      </ul>").append(LS);
-		buffer.append("    </div>-->").append(LS);
-		buffer.append("  </header>").append(LS);
-		// @formatter:on
-		return buffer;
-	}
-
-	/**
-	 * Computes the content of the toc.xml file.
-	 * 
-	 * @param serviceProviders
-	 *            The list of Java classes providing services to AQL.
-	 * @return The content of the toc.xml
-	 */
-	public static StringBuffer computeToc(Class<?>... serviceProviders) {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>").append(LS);
-		buffer.append("<?NLS TYPE=\"org.eclipse.help.toc\"?>").append(LS);
-		buffer.append("<toc label=\"Acceleo Query Language Documentation\" topic=\"pages/index.html\">")
-				.append(LS);
-		for (Class<?> serviceProviderClass : serviceProviders) {
-			if (serviceProviderClass.isAnnotationPresent(ServiceProvider.class)) {
-				ServiceProvider serviceProvider = serviceProviderClass.getAnnotation(ServiceProvider.class);
-				buffer.append("<topic href=\"pages/" + AQL_HREF_PREFIX + serviceProviderClass.getSimpleName()
-						.toLowerCase() + ".html\" label=\"" + serviceProvider.value() + "\"></topic>").append(
-								LS);
-			}
-		}
-		buffer.append("</toc>").append(LS);
-		return buffer;
-	}
-
-	/**
-	 * Produces the content of the AQL Overview page.
-	 * 
-	 * @return The sections to display in the page
-	 */
-	public static List<StringBuffer> computeAQLOverviewSections() {
-		List<StringBuffer> buffers = new ArrayList<StringBuffer>();
-
-		// @formatter:off
-		StringBuffer introductionSection = new StringBuffer();
-		introductionSection.append("  <div class=\"jumbotron masthead\">").append(LS);
-		introductionSection.append("    <h1>Acceleo Query Language</h1>").append(LS);
-		introductionSection.append("  </div>").append(LS);
-		introductionSection.append("  <div class=\"marketing\">").append(LS);
-		introductionSection.append("    <h1>Query and navigate in EMF models</h1>").append(LS);
-		introductionSection.append("  </div>").append(LS);
-		introductionSection.append("").append(LS);
-		introductionSection.append("  <section id=\"overview\">").append(LS);
-		introductionSection.append("    <div class=\"page-header\">").append(LS);
-		introductionSection.append("      <h1>Overview</h1>").append(LS);
-		introductionSection.append("    </div>").append(LS);
-		introductionSection.append("    <p>").append(LS);
-		introductionSection.append("      The Acceleo Query Language (AQL) is a language used to navigate and query an EMF model. In this document, you will find the description ").append(LS);
-		introductionSection.append("      of all the services of the standard library of AQL.").append(LS);
-		introductionSection.append("    </p>").append(LS);
-		introductionSection.append("  </section>").append(LS);
-		// @formatter:on
-
-		buffers.add(introductionSection);
-
-		return buffers;
-	}
-
-	/**
-	 * Computes the sections for a service provider.
-	 * 
-	 * @param serviceProviderClass
-	 *            The service provider
-	 * @return The sections to display in the HTML page
-	 */
-	public static List<StringBuffer> computeServiceSections(Class<?> serviceProviderClass) {
-		return computeServiceSections(serviceProviderClass, 1, METHODE_SIGNATURE_OLD_GENERATOR);
 	}
 
 	/**
@@ -423,7 +169,7 @@ public final class AQLHelpContentUtils {
 	 *            the signature generator {@link Function}
 	 * @return The sections to display in the HTML page
 	 */
-	public static List<StringBuffer> computeServiceSections(Class<?> serviceProviderClass, int titleLevel,
+	public static List<StringBuffer> computeServiceSections(Class<?> serviceProviderClass,
 			Function<Method, StringBuffer> signatureGenerator) {
 		List<StringBuffer> buffers = new ArrayList<StringBuffer>();
 
@@ -432,13 +178,8 @@ public final class AQLHelpContentUtils {
 			return buffers;
 		}
 
-	// @formatter:off
 		StringBuffer servicesSection = new StringBuffer();
-		servicesSection.append("  <section id=\"services\">").append(LS);
-		servicesSection.append("    <div class=\"page-header\">").append(LS);
-		servicesSection.append("      <h" + titleLevel + ">").append(serviceProvider.value()).append("</h" + titleLevel + ">").append(LS);
-		servicesSection.append("    </div>").append(LS);
-		// @formatter:on
+		servicesSection.append("=== ").append(serviceProvider.value()).append(LS).append(LS);
 
 		Method[] methods = serviceProviderClass.getMethods();
 
@@ -455,76 +196,52 @@ public final class AQLHelpContentUtils {
 			if (method.isAnnotationPresent(Documentation.class)) {
 				Documentation serviceDocumentation = method.getAnnotation(Documentation.class);
 
-			// @formatter:off
 				servicesSection.append(LS);
 
-				servicesSection.append("        <h3>").append(signatureGenerator.apply(method)).append("</h3>").append(LS);
-				servicesSection.append("        <p>").append(LS);
-				servicesSection.append("          ").append(serviceDocumentation.value()).append(LS);
-				servicesSection.append("        </p>").append(LS);
-				
-				if (serviceDocumentation.examples().length > 0) {					
-					servicesSection.append("        <table class=\"table table-striped table-bordered table-condensed\">").append(LS);
-					servicesSection.append("          <thead>").append(LS);
-					servicesSection.append("            <tr>").append(LS);
-					servicesSection.append("              <th>Expression</th>").append(LS);
-					servicesSection.append("              <th>Result</th>").append(LS);
-					servicesSection.append("            </tr>").append(LS);
-					servicesSection.append("          </thead><colgroup><col width=\"60%\" /><col width=\"40%\" /></colgroup>").append(LS);
-					servicesSection.append("          <tbody>").append(LS);
-					
+				servicesSection.append("==== ").append(signatureGenerator.apply(method)).append(LS).append(
+						LS);
+				servicesSection.append(serviceDocumentation.value()).append(LS).append(LS);
+				servicesSection.append("Parameters:").append(LS).append(LS);
+				if (serviceDocumentation.params().length > 0) {
+					for (Param param : serviceDocumentation.params()) {
+						servicesSection.append("* *").append(param.name()).append("*: ").append(param.value())
+								.append(LS);
+					}
+					servicesSection.append(LS);
+				}
+				if (serviceDocumentation.examples().length > 0) {
+					servicesSection.append("|===").append(LS);
+					servicesSection.append("| *Expression* | *Result*").append(LS);
+
 					List<Other> others = new ArrayList<Other>();
-					
+
 					Example[] examples = serviceDocumentation.examples();
-					for (Example example : examples) {						
-						servicesSection.append("            <tr>").append(LS);
-						servicesSection.append("              <td>").append(example.expression()).append("</td>").append(LS);
-						servicesSection.append("              <td>").append(example.result()).append("</td>").append(LS);
-						servicesSection.append("            </tr>").append(LS);
-						
+					for (Example example : examples) {
+						servicesSection.append("| `").append(example.expression().replace("|", "\\|")).append(
+								"` | `").append(example.result().replace("|", "\\|")).append("`").append(LS);
+
 						Other[] otherExamples = example.others();
 						for (Other otherExample : otherExamples) {
 							others.add(otherExample);
 						}
 					}
-					
-					servicesSection.append("          </tbody>").append(LS);
-					servicesSection.append("        </table>").append(LS);
-					
-					servicesSection.append("        <p>").append(LS);
-					servicesSection.append("          ").append(serviceDocumentation.comment()).append(LS);
-					servicesSection.append("        </p>").append(LS);
-					
+					servicesSection.append("|===").append(LS).append(LS);
+
 					if (others.size() > 0) {
-						servicesSection.append("        <h4>In other languages</h4>").append(LS);
-						servicesSection.append("        <table class=\"table table-striped table-bordered table-condensed\">").append(LS);
-						servicesSection.append("          <thead>").append(LS);
-						servicesSection.append("            <tr>").append(LS);
-						servicesSection.append("              <th>Language</th>").append(LS);
-						servicesSection.append("              <th>Expression</th>").append(LS);
-						servicesSection.append("              <th>Result</th>").append(LS);
-						servicesSection.append("            </tr>").append(LS);
-						servicesSection.append("          </thead><colgroup><col width=\"60%\" /><col width=\"40%\" /></colgroup>").append(LS);
-						servicesSection.append("          <tbody>").append(LS);
+						servicesSection.append("|===").append(LS);
+						servicesSection.append("*In other languages*").append(LS).append(LS);
+						servicesSection.append("| *Language* | *Expression* | *Result* |").append(LS);
 						for (Other alternate : others) {
-							servicesSection.append("            <tr>").append(LS);
-							servicesSection.append("              <td>").append(alternate.language()).append("</td>").append(LS);
-							servicesSection.append("              <td>").append(alternate.expression()).append("</td>").append(LS);
-							servicesSection.append("              <td>").append(alternate.result()).append("</td>").append(LS);
-							servicesSection.append("            </tr>").append(LS);
+							servicesSection.append("| `").append(alternate.language().replace("|", "\\|"))
+									.append("` | `").append(alternate.expression().replace("|", "\\|"))
+									.append(" | ").append(alternate.result().replace("|", "\\|")).append("`")
+									.append(LS);
 						}
-						servicesSection.append("          </tbody>").append(LS);
-						servicesSection.append("        </table>").append(LS);
+						servicesSection.append("|===").append(LS).append(LS);
 					}
 				}
-				servicesSection.append("        <hr />").append(LS);
-				// @formatter:on
 			}
 		}
-
-	// @formatter:off
-		servicesSection.append("  </section>").append(LS);
-		// @formatter:on
 
 		buffers.add(servicesSection);
 
--- query/plugins/org.eclipse.acceleo.query.doc/src/org/eclipse/acceleo/query/doc/internal/DocumentationGenerator.java
+++ query/plugins/org.eclipse.acceleo.query.doc/src/org/eclipse/acceleo/query/doc/internal/DocumentationGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2023 Obeo.
+ * Copyright (c) 2015, 2025 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -9,11 +9,6 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 package org.eclipse.acceleo.query.doc.internal;
-
-import static org.eclipse.acceleo.query.doc.internal.AQLHelpContentUtils.body;
-import static org.eclipse.acceleo.query.doc.internal.AQLHelpContentUtils.head;
-import static org.eclipse.acceleo.query.doc.internal.AQLHelpContentUtils.header;
-import static org.eclipse.acceleo.query.doc.internal.AQLHelpContentUtils.html;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -45,9 +40,14 @@ import org.eclipse.acceleo.query.services.XPathServices;
 public final class DocumentationGenerator {
 
 	/**
-	 * Replacement marker.
+	 * Textile service section start tag.
 	 */
-	private static final String TRIGGER_TO_APPEND_STD_DOC = "<p>These sections are listing all the services of the standard library of AQL.</p>";
+	private static final String SERVICES_START_ASCIIDOC_TAG = "// SERVICES START"; //$NON-NLS-1$
+
+	/**
+	 * Textile service section end tag.
+	 */
+	private static final String SERVICES_END_ASCIIDOC_TAG = "// SERVICES END"; //$NON-NLS-1$
 
 	/**
 	 * The name of the charset to use to write the documentation.
@@ -81,71 +81,43 @@ public final class DocumentationGenerator {
 		System.out.println("Prepare the generation of the documentation for " + pluginFolder
 				.getAbsolutePath());
 
-		File inputFolder = new File(pluginFolder, "input"); //$NON-NLS-1$
-		File documentationFolder = new File(pluginFolder, "pages"); //$NON-NLS-1$
-		File indexHtmlFile = new File(documentationFolder, "index.html"); //$NON-NLS-1$
-
-		// toc.xml
-		StringBuffer buffer = AQLHelpContentUtils.computeToc(STANDARD_SERVICE_PROVIDERS);
-		try {
-			File tocFile = new File(pluginFolder, "toc.xml");
-			System.out.println("Writing the content of toc.xml in " + tocFile.getAbsolutePath());
-			write(buffer.toString(), tocFile, Charset.forName(UTF8));
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
+		File pagesFolder = new File(pluginFolder, "pages"); //$NON-NLS-1$
+		File indexAsciidocFile = new File(pagesFolder, "index.adoc"); //$NON-NLS-1$
 
 		// Services
 		StringBuffer aggregated = new StringBuffer();
 		for (Class<?> serviceProviderClass : STANDARD_SERVICE_PROVIDERS) {
 			if (serviceProviderClass.isAnnotationPresent(ServiceProvider.class)) {
-				try {
-					List<StringBuffer> sections = AQLHelpContentUtils.computeServiceSections(
-							serviceProviderClass);
-					StringBuffer stringBuffer = html(head(), body(header(false), sections));
-
-					File file = new File(documentationFolder, AQLHelpContentUtils.AQL_HREF_PREFIX
-							+ serviceProviderClass.getSimpleName().toLowerCase() + ".html");
-					System.out.println("Writing content of " + file.getAbsolutePath());
-					write(stringBuffer.toString(), file, Charset.forName(UTF8));
-
-					/*
-					 * generating a documentation aggregating all the services at once.
-					 */
-					List<StringBuffer> sectionsForAggregatedServices = AQLHelpContentUtils
-							.computeServiceSections(serviceProviderClass, 3,
-									AQLHelpContentUtils.METHOD_SIGNATURE_GENERATOR_2016);
-					for (StringBuffer b : sectionsForAggregatedServices) {
-						aggregated.append(b);
-					}
-
-				} catch (IOException exception) {
-					exception.printStackTrace();
+				/*
+				 * generating a documentation aggregating all the services at once.
+				 */
+				List<StringBuffer> sectionsForAggregatedServices = AQLHelpContentUtils.computeServiceSections(
+						serviceProviderClass, AQLHelpContentUtils.METHOD_SIGNATURE_GENERATOR_2016);
+				for (StringBuffer b : sectionsForAggregatedServices) {
+					aggregated.append(b);
 				}
 			}
 		}
 
-		// index.html
+		// index.adoc
 		try {
-			List<StringBuffer> sections = AQLHelpContentUtils.computeAQLOverviewSections();
+			String inputAsciidocContent = read(indexAsciidocFile, Charset.forName(UTF8));
 
-			String inputHtmlContent = read(new File(inputFolder, "index.html"), Charset.forName(UTF8));
-			int indexOfBodyStart = inputHtmlContent.indexOf("<body>");
-			if (indexOfBodyStart != -1 && indexOfBodyStart + 6 < inputHtmlContent.length()) {
-				inputHtmlContent = inputHtmlContent.substring(indexOfBodyStart + 6);
-			}
-			int indexOfBodyEnd = inputHtmlContent.indexOf("</body>");
-			if (indexOfBodyEnd != -1) {
-				inputHtmlContent = inputHtmlContent.substring(0, indexOfBodyEnd);
-			}
-			sections.add(new StringBuffer(inputHtmlContent));
+			StringBuffer buffer = new StringBuffer(inputAsciidocContent.length());
 
-			StringBuffer stringBuffer = html(head(), body(header(true), sections));
-			String out = stringBuffer.toString().replace(TRIGGER_TO_APPEND_STD_DOC, TRIGGER_TO_APPEND_STD_DOC
-					+ "\n" + aggregated);
+			int serviceSectionStartIndex = inputAsciidocContent.indexOf(SERVICES_START_ASCIIDOC_TAG)
+					+ SERVICES_START_ASCIIDOC_TAG.length();
+			int serviceSectionEndIndex = inputAsciidocContent.indexOf(SERVICES_END_ASCIIDOC_TAG);
+			buffer.append(inputAsciidocContent.substring(0, serviceSectionStartIndex));
+			buffer.append(AQLHelpContentUtils.LS);
+			buffer.append(AQLHelpContentUtils.LS);
+			buffer.append(aggregated);
+			buffer.append(inputAsciidocContent.substring(serviceSectionEndIndex, inputAsciidocContent
+					.length()));
+			String out = buffer.toString();
 
-			System.out.println("Writing content of " + indexHtmlFile.getAbsolutePath());
-			write(out, indexHtmlFile, Charset.forName(UTF8));
+			System.out.println("Writing content of " + indexAsciidocFile.getAbsolutePath());
+			write(out, indexAsciidocFile, Charset.forName(UTF8));
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
