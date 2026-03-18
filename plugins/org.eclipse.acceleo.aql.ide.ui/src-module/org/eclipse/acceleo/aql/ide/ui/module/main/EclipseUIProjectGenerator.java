56d5f1adfbe52a4c27b0618926738d99c046da58
Yvan Lussaud <yvan.lussaud@obeo.fr>
2026-03-18 16:12:22 +0100

    Fixed EclipseUIProjectGenerator to have the parameter Java type.

--- plugins/org.eclipse.acceleo.aql.ide.ui/src-module/org/eclipse/acceleo/aql/ide/ui/module/main/EclipseUIProjectGenerator.java
+++ plugins/org.eclipse.acceleo.aql.ide.ui/src-module/org/eclipse/acceleo/aql/ide/ui/module/main/EclipseUIProjectGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024, 2025 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -154,7 +154,7 @@ public class EclipseUIProjectGenerator extends AbstractGenerator {
 			final Module modelModule = loadModelModule(URI.createFileURI(moduleAbsolutePath),
 					modelModuleQualifiedName);
 			modelModules.add(modelModule);
-			final Set<String> modelNsURIs = AQLUtils.getAllNeededEPackages(resolver,
+			final Set<String> modelNsURIs = AQLUtils.getAllNeededEPackages(workspaceResolver,
 					modelModuleQualifiedName);
 			AQLUtils.registerEPackages(queryEnvironment, EPackage.Registry.INSTANCE, modelNsURIs);
 		}
